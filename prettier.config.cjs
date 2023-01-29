d9eb5bebf28eebd31ae527d3c5bd80b231fcf00b
Daniel Thorp <arcanistzed@gmail.com>
2023-01-29 00:12:12 -0500

    feat: working database& prisma
    Signed-off-by: Daniel Thorp <arcanistzed@gmail.com>

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,4 +1,13 @@
 /** @type {import("prettier").Config} */
 module.exports = {
   plugins: [require.resolve("prettier-plugin-tailwindcss")],
+  printWidth: 120,
+  tabWidth: 4,
+  semi: true,
+  singleQuote: false,
+  trailingComma: "all",
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: "avoid",
+  proseWrap: "preserve"
 };
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,19 +2,16 @@ import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 
-import { api } from "../utils/api";
+import { trpc } from "../utils/api";
 
 import "../styles/globals.css";
 
-const MyApp: AppType<{ session: Session | null }> = ({
-  Component,
-  pageProps: { session, ...pageProps },
-}) => {
-  return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
-  );
+const MyApp: AppType<{ session: Session | null }> = ({ Component, pageProps: { session, ...pageProps } }) => {
+    return (
+        <SessionProvider session={session}>
+            <Component {...pageProps} />
+        </SessionProvider>
+    );
 };
 
-export default api.withTRPC(MyApp);
+export default trpc.withTRPC(MyApp);
