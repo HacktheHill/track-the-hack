36e22d8e79f5b8e23167436f16bfb189796b1520
Daniel Thorp <arcanistzed@gmail.com>
2023-01-30 04:07:47 -0500

    style: formatting
    Signed-off-by: Daniel Thorp <arcanistzed@gmail.com>

--- src/components/QRCode.tsx
+++ src/components/QRCode.tsx
@@ -4,28 +4,28 @@ import qrcode from "qrcode";
 import { useEffect, useState } from "react";
 
 const QRCode = () => {
-    const { data: sessionData } = useSession();
-    const [qrCode, setQRCode] = useState<string | null>(null);
+	const { data: sessionData } = useSession();
+	const [qrCode, setQRCode] = useState<string | null>(null);
 
-    useEffect(() => {
-        async function getQRCode() {
-            if (sessionData) {
-                if (!sessionData.user) {
-                    throw new Error("No user");
-                }
-                try {
-                    const qr = await qrcode.toDataURL(sessionData.user.id);
-                    setQRCode(qr);
-                } catch (error) {
-                    console.error(error);
-                }
-            }
-        }
-        void getQRCode();
-    }, [sessionData]);
+	useEffect(() => {
+		async function getQRCode() {
+			if (sessionData) {
+				if (!sessionData.user) {
+					throw new Error("No user");
+				}
+				try {
+					const qr = await qrcode.toDataURL(sessionData.user.id);
+					setQRCode(qr);
+				} catch (error) {
+					console.error(error);
+				}
+			}
+		}
+		void getQRCode();
+	}, [sessionData]);
 
-    if (!qrCode) return null;
-    return <Image src={qrCode} alt="QR Code" width={200} height={200} />;
+	if (!qrCode) return null;
+	return <Image src={qrCode} alt="QR Code" width={200} height={200} />;
 };
 
 export default QRCode;
--- src/components/QRScanner.tsx
+++ src/components/QRScanner.tsx
@@ -2,28 +2,28 @@ import QrScanner from "qr-scanner";
 import { useEffect, useRef } from "react";
 
 type QRScannerProps = {
-    setId: (id: string) => void;
+	setId: (id: string) => void;
 };
 
 const QRScanner = ({ setId }: QRScannerProps) => {
-    const video = useRef<HTMLVideoElement>(null);
+	const video = useRef<HTMLVideoElement>(null);
 
-    useEffect(() => {
-        if (!video.current) return;
-        const qrScanner = new QrScanner(
-            video.current,
-            (result: QrScanner.ScanResult) => {
-                setId(result.data);
-                console.log(result.data);
-            },
-            {
-                returnDetailedScanResult: true,
-                highlightCodeOutline: true,
-            },
-        );
-        void qrScanner.start();
-    });
-    return <video ref={video} width="300" height="300" />;
+	useEffect(() => {
+		if (!video.current) return;
+		const qrScanner = new QrScanner(
+			video.current,
+			(result: QrScanner.ScanResult) => {
+				setId(result.data);
+				console.log(result.data);
+			},
+			{
+				returnDetailedScanResult: true,
+				highlightCodeOutline: true,
+			},
+		);
+		void qrScanner.start();
+	});
+	return <video ref={video} width="300" height="300" />;
 };
 
 export default QRScanner;
