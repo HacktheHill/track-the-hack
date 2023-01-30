3d15800ec56db7712be3ec5e0c76ccff48db796d
Daniel Thorp <arcanistzed@gmail.com>
2023-01-30 04:07:47 -0500

    style: formatting
    Signed-off-by: Daniel Thorp <arcanistzed@gmail.com>

--- prisma/prisma.d.ts
+++ prisma/prisma.d.ts
@@ -1,21 +1,21 @@
 import { PrismaClient } from "@prisma/client";
 declare module "@prisma/client" {
-    export interface AttendanceType {
-        IN_PERSON: "IN_PERSON";
-        ONLINE: "ONLINE";
-    }
-    export interface Language {
-        EN: "EN";
-        FR: "FR";
-    }
-    export interface ShirtSize {
-        S: "S";
-        M: "M";
-        L: "L";
-        XL: "XL";
-    }
-    export const AttendanceType: AttendanceType;
-    export const Language: Language;
-    export const ShirtSize: ShirtSize;
-    export default PrismaClient;
+	export interface AttendanceType {
+		IN_PERSON: "IN_PERSON";
+		ONLINE: "ONLINE";
+	}
+	export interface Language {
+		EN: "EN";
+		FR: "FR";
+	}
+	export interface ShirtSize {
+		S: "S";
+		M: "M";
+		L: "L";
+		XL: "XL";
+	}
+	export const AttendanceType: AttendanceType;
+	export const Language: Language;
+	export const ShirtSize: ShirtSize;
+	export default PrismaClient;
 }
