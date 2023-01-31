8df7dd5bc76c2e3348ef246fb606da13cd801828
Daniel Thorp <arcanistzed@gmail.com>
2023-01-30 23:09:52 -0500

    fix: remove broken typescript files
    Signed-off-by: Daniel Thorp <arcanistzed@gmail.com>

--- src/types/i18next.d.ts
+++ src/types/i18next.d.ts
@@ -1,10 +1,7 @@
 import "i18next";
-import type { defaultNS, resources } from "./i18n";
 
 declare module "i18next" {
 	interface CustomTypeOptions {
 		returnNull: false;
-		resources: (typeof resources)["en"];
-		defaultNS: typeof defaultNS;
 	}
 }
