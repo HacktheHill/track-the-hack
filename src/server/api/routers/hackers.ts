6cbc11a2d4e99751a5fdd1cf62ad3df3b1a1541c
Daniel Thorp <arcanistzed@gmail.com>
2023-02-04 20:09:47 -0500

    refactor: move unsubscribe page
    Also extracted the common logic and markup into a single component
    
    Signed-off-by: Daniel Thorp <arcanistzed@gmail.com>

--- src/server/api/routers/hackers.ts
+++ src/server/api/routers/hackers.ts
@@ -3,13 +3,29 @@ import { z } from "zod";
 import { createTRPCRouter, protectedProcedure, publicProcedure } from "../trpc";
 
 export const hackerRouter = createTRPCRouter({
-	get: publicProcedure.input(z.object({ id: z.string() })).query(async ({ ctx, input }) => {
-		return ctx.prisma.hackerInfo.findUnique({
-			where: {
-				id: input.id,
-			},
-		});
-	}),
+	get: publicProcedure
+		.input(
+			z
+				.object({
+					id: z.string(),
+				})
+				.or(z.object({ email: z.string() })),
+		)
+		.query(async ({ ctx, input }) => {
+			if ("id" in input) {
+				return ctx.prisma.hackerInfo.findUnique({
+					where: {
+						id: input.id,
+					},
+				});
+			} else if ("email" in input) {
+				return ctx.prisma.hackerInfo.findFirst({
+					where: {
+						email: input.email,
+					},
+				});
+			}
+		}),
 	assign: protectedProcedure
 		.input(
 			z.object({
@@ -19,7 +35,17 @@ export const hackerRouter = createTRPCRouter({
 				userId: z.string(),
 			}),
 		)
-		.mutation(({ ctx, input }) => {
+		.mutation(async ({ ctx, input }) => {
+			const hacker = await ctx.prisma.hackerInfo.findUnique({
+				where: {
+					id: input.id,
+				},
+			});
+
+			if (!hacker) {
+				throw new Error("Hacker not found");
+			}
+
 			return ctx.prisma.hackerInfo.update({
 				where: {
 					id: input.id,
@@ -29,6 +55,42 @@ export const hackerRouter = createTRPCRouter({
 					attendanceType: input.attendanceType,
 					userId: input.userId,
 					confirmed: true,
+				},
+			});
+		}),
+	unsubscribe: publicProcedure
+		.input(
+			z.object({
+				email: z.string(),
+				unsubscribeToken: z.string().nullable(),
+				unsubscribe: z.boolean().default(true),
+			}),
+		)
+		.mutation(async ({ ctx, input }) => {
+			const hacker = await ctx.prisma.hackerInfo.findFirst({
+				where: {
+					email: input.email,
+				},
+			});
+
+			if (!hacker) {
+				throw new Error("Hacker not found");
+			}
+
+			// If the hacker has an unsubscribe token and is not using it or is using it incorrectly
+			if (
+				hacker.unsubscribeToken !== null &&
+				(input.unsubscribeToken === null || hacker.unsubscribeToken !== input.unsubscribeToken)
+			) {
+				throw new Error("invalid-unsubscribe-token");
+			}
+
+			return ctx.prisma.hackerInfo.updateMany({
+				where: {
+					email: input.email,
+				},
+				data: {
+					unsubscribed: input.unsubscribe,
 				},
 			});
 		}),
