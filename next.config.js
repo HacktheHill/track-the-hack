3d028103c72ec48affe4a7492ce7385ae584615d
Daniel Thorp <arcanistzed@gmail.com>
2023-02-21 03:18:32 -0500

    style: remove unnecessary SVGR
    Signed-off-by: Daniel Thorp <arcanistzed@gmail.com>

--- next.config.js
+++ next.config.js
@@ -24,13 +24,4 @@ module.exports = withPWA({
 	},
 	swcMinify: true,
 	i18n,
-	webpack(config) {
-		config.module.rules.push({
-			test: /\.svg$/i,
-			issuer: /\.[jt]sx?$/,
-			use: ["@svgr/webpack"],
-		});
-
-		return config;
-	},
 });
