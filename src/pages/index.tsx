2cde2dbe21c3029919576b3b237366771af4b341
Daniel Thorp <arcanistzed@gmail.com>
2023-02-21 17:06:46 -0500

    feat: description and weather opacity
    Signed-off-by: Daniel Thorp <arcanistzed@gmail.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -45,11 +45,12 @@ const Home: NextPage = () => {
 	}, [query.data, router]);
 
 	return (
-		<App className="relative flex h-full flex-col items-center justify-center gap-12 bg-gradient1 px-16 py-12">
+		<App className="relative flex flex-col items-center justify-center gap-8 bg-gradient1 px-16 py-12">
 			<Weather count={25} type="snowflake" />
 			<Weather count={3} type="cloud" />
-			<Image priority className="z-10" src="/assets/mascot-waving.svg" alt="Mascot" width={400} height={400} />
+			<Image priority className="z-10" src="/assets/mascot-waving.svg" alt="Mascot" width={225} height={225} />
 			<p className="z-10 text-center text-2xl font-bold">{t("welcome")}</p>
+			<p className="text-l z-10 max-w-xl text-center">{t("description")}</p>
 			<button
 				className="z-10 whitespace-nowrap rounded border border-dark bg-background1 py-2 px-4 font-coolvetica text-dark transition-colors hover:bg-background3"
 				onClick={sessionData ? () => void signOut() : () => void signIn()}
