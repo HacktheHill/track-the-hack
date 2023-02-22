f6e081f896034613c01b791a8abedc82fc3a263c
Arjuunan Arulnamby <arjuunan99@gmail.com>
2023-02-22 00:27:13 -0500

    Update event.tsx

--- src/pages/schedule/event.tsx
+++ src/pages/schedule/event.tsx
@@ -41,7 +41,7 @@ const Event = () => {
 	}
 
 	return (
-		<App className="flex h-full flex-col text-center items-center justify-start gap-4 text-dark bg-background1">
+		<App className="flex h-full w-full flex-col text-center items-center justify-start gap-4 text-dark bg-background1">
 			<EventDisplay {...query.data} />
 		</App>
 	);
@@ -52,7 +52,19 @@ type EventProps = Prisma.EventGetPayload<true>;
 const EventDisplay = ({ id, name, type, description, start, end, host, room, tiktok }: EventProps) => {
 	return (
 		<>
-			<div className="flex flex-col mt-4 font-[Coolvetica]">
+			<Link
+				key={id}
+				href={"/schedule"}
+				className={'absolute items-right justify-start p-2'}
+				style={{marginLeft:"85%"}}
+				>
+					<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+						<title>Close Event</title>
+						<path d="M18 6L6 18" stroke="#3B477A" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+						<path d="M6 6L18 18" stroke="#3B477A" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+					</svg>
+			</Link>
+			<div className="flex flex-col mt-2 font-[Coolvetica]">
 				<h1 className="text-xl">{name}</h1>
 				<p className="text-lg">
 					{start.getHours().toString()}:{start.getMinutes().toString()}&nbsp;-
@@ -60,29 +72,39 @@ const EventDisplay = ({ id, name, type, description, start, end, host, room, tik
 				</p>
 				<p className="text-md">{room}</p>
 			</div>
-			<Image src="" width={300} height={300} alt=""></Image>
-			<div className="flex flex-col font-[Coolvetica]">
-				<h1 className="text-xl">{description}</h1>
-				<p className="text-lg">{type}</p>
-				<p className="text-md">{host}</p>
+
+			<hr style={{background: "#3B477A",height: "1px",border: "none",width: "100%"}}/>
+
+			<div style={{borderRadius: '20px', overflow: 'hidden'}}>
+				<Image src="/icons/android-chrome-512x512.png" width={350} height={325} alt=""></Image>
 			</div>
+
+			<div className="flex flex-col font-[Coolvetica] mb-3">
+				<h1 className="text-xl">{description}</h1>
+				<p className="text-md">{type}</p>
+				<p className="text-sm">{host}</p>
+			</div>
+
 			<Link
 				key={id}
 				href={''}
 				target="_blank" 
 				rel="noreferrer"
-				className={`flex flex-col items-center justify-center 
-				gap-2 rounded-xl p-5 px-20 mt-5 font-[Coolvetica] text-dark bg-white`}
+				className={`flex flex-row items-center justify-center 
+				gap-2 rounded-xl p-3 font-[Coolvetica] text-dark bg-white`}
 			>
-				<h1 className="text-xl">tiktok video link</h1>
+				<div style={{backgroundColor:'#90A1D4', height: '4rem', width: '4rem', borderRadius: '13px', overflow: 'hidden'}}/>
+				<h1 className="text-xl pl-7 pr-14 mr-7">tiktok video link</h1>
 			</Link>
+
 			<Link
 				key={id}
-				href={''}
-				className={`flex flex-col items-center justify-center 
-				gap-2 rounded-xl p-5 px-20 font-[Coolvetica] text-dark bg-white`}
+				href={'/resources'}
+				className={`flex flex-row items-center justify-center 
+				gap-2 rounded-xl p-3 font-[Coolvetica] text-dark bg-white`}
 			>
-				<h1 className="text-xl">&nbsp;&nbsp;resources link&nbsp;&nbsp;</h1>
+				<div style={{backgroundColor:'#90A1D4', height: '4rem', width: '4rem', borderRadius: '13px', overflow: 'hidden'}}/>
+				<h1 className="text-xl pl-10 pr-14 mr-9">resources link</h1>
 			</Link>
 		</>
 	);
