a88e6421645cf83dbf5fe9b149a13d0a8b2b6df7
Daniel Thorp <arcanistzed@gmail.com>
2023-02-22 23:56:36 -0500

    feat: small adjustments to hackers page
    Signed-off-by: Daniel Thorp <arcanistzed@gmail.com>

--- src/pages/hackers/index.tsx
+++ src/pages/hackers/index.tsx
@@ -1,18 +1,26 @@
-
-import Card from '../../components/Card';
-import Search from '../../components/Search';
-import App from "../../components/App";
-
+import { useRouter } from "next/router";
 import { trpc } from "../../utils/api";
+import Hacker from "./hacker";
+import type { GetStaticProps } from "next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import type { HackerInfo } from "@prisma/client";
+import Link from "next/link";
+
+import App from "../../components/App";
 import Error from "../../components/Error";
 import Loading from "../../components/Loading";
-import { useRouter,  } from 'next/router';
-import Hacker from "./hacker";
-import { useEffect, useState } from 'react';
+
+export const getStaticProps: GetStaticProps = async ({ locale }) => {
+	return {
+		props: await serverSideTranslations(locale ?? "en", ["common", "hackers"]),
+	};
+};
+
 const Hackers = () => {
-  
-  const query = trpc.hackers.all.useQuery();
-  const router = useRouter();
+	const router = useRouter();
+	const [id] = [router.query.id].flat();
+
+	const query = trpc.hackers.all.useQuery();
 
 	if (query.isLoading || query.data == null) {
 		return (
@@ -34,21 +42,82 @@ const Hackers = () => {
 		void router.push("/404");
 	}
 
-  const [id] = [router.query.id].flat()
+	return id ? (
+		<Hacker />
+	) : (
+		<App className="flex h-full flex-col gap-8 overflow-y-auto bg-gradient-to-b from-background2 to-background1 py-8 px-4 sm:px-20">
+			<Search></Search>
+			<div className="to-mobile:mx-auto grid h-full grid-cols-2 flex-col gap-8 overflow-y-auto p-4 sm:grid-cols-2 lg:grid-cols-3">
+				{query.data.map(hacker => (
+					<Card
+						key={hacker.id}
+						id={hacker.id}
+						firstName={hacker.firstName}
+						lastName={hacker.lastName}
+						university={hacker.university}
+						studyProgram={hacker.studyProgram}
+					/>
+				))}
+			</div>
+		</App>
+	);
+};
 
-  return (
-    id?
-    <Hacker/>
-    :
-    <App className="h-full bg-gradient-to-b from-background2 to-background1  py-8 sm:px-20">
-      <Search></Search>
-				<div className="h-full flex-col gap-8 overflow-y-auto to-mobile:mx-auto grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3">
-          {query.data.map((hacker) => (
-            <Card key={hacker.id} id={hacker.id} firstName={hacker.firstName} lastName={hacker.lastName} university={hacker.university} studyProgram={hacker.studyProgram}/>
-          ))}
-        </div>
-	  </App> 
-)
-}
+type CardProps = Pick<HackerInfo, "university" | "firstName" | "lastName" | "studyProgram" | "id">;
 
-export default Hackers
+const Card = ({ firstName, lastName, university, studyProgram, id }: CardProps) => {
+	return (
+		<Link
+			href={`/hackers?id=${id}`}
+			className="block w-full rounded-lg bg-dark p-6 text-white shadow hover:bg-medium"
+		>
+			<h3 className="text-2xl font-bold tracking-tight">{`${firstName} ${lastName}`}</h3>
+			<p>{university}</p>
+			<p>{studyProgram}</p>
+		</Link>
+	);
+};
+
+const Search = () => {
+	return (
+		<div className="flex flex-col gap-3.5" id="bubble-table-select">
+			<form onSubmit={event => event.preventDefault()}>
+				<label className="sr-only mb-2 text-sm font-medium">Search</label>
+				<div className="relative">
+					<div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+						<svg
+							aria-hidden="true"
+							className="h-5 w-5"
+							fill="none"
+							stroke="currentColor"
+							viewBox="0 0 24 24"
+							xmlns="http://www.w3.org/2000/svg"
+						>
+							<path
+								strokeLinecap="round"
+								strokeLinejoin="round"
+								strokeWidth="2"
+								d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+							></path>
+						</svg>
+					</div>
+					<input
+						type="search"
+						id="default-search"
+						className="block w-full rounded-lg bg-background1 p-4 pl-10 text-sm"
+						placeholder="Search Hackers"
+						required
+					/>
+					<button
+						type="submit"
+						className="absolute right-2 bottom-2 rounded-lg bg-dark px-4 py-2 text-sm font-medium text-white hover:bg-medium focus:outline-none focus:ring-4"
+					>
+						Search
+					</button>
+				</div>
+			</form>
+		</div>
+	);
+};
+
+export default Hackers;
