f415afe6373e841f7a73e7f7ab28edd561354ab6
Emilien Breton <bricktech2000@gmail.com>
2023-02-22 20:27:56 -0500

    feat: hacker view component styles

--- src/pages/hackers/hacker.tsx
+++ src/pages/hackers/hacker.tsx
@@ -89,39 +89,117 @@ const HackerView = ({ hackerData, presenceData: { id: _, ...presenceData } }: Ha
 	const presenceMutation = trpc.presence.update.useMutation();
 	const [presenceState, setPresenceState] = useState(presenceData);
 
+	const paragraphClass = "flex justify-between gap-4 text-right";
+	const boldClass = "text-left font-bold";
+
 	return (
 		// overflow auto
-		<div className="flex flex-col overflow-auto">
-			<h1 className="font-coolvetica text-[clamp(1rem,3.5vmin,5rem)] font-normal text-dark">
-				{hackerData.firstName} {hackerData.lastName}
+		<div className="mx-auto flex max-w-2xl flex-col gap-4 p-4 pt-10 pb-10">
+			<h1 className="self-center font-coolvetica text-4xl font-normal text-dark">
+				{hackerData.firstName} {hackerData.lastName} ({hackerData.gender})
 			</h1>
+			<p className="self-center">
+				{hackerData.studyLevel} {hackerData.studyProgram} at {hackerData.university} (
+				{hackerData.graduationYear})
+			</p>
 			<OnlyRole roles={[Role.ORGANIZER]}>
-				{Object.entries(presenceState).map(([key, value]) => (
-					<p key={key}>
-						<input
-							type="checkbox"
-							key={key}
-							id={key}
-							checked={value}
-							onChange={() => {
-								const updatedPresenceInfo = { ...presenceState, [key]: !value };
-								setPresenceState(updatedPresenceInfo);
-								presenceMutation.mutate({
-									id: id ?? "",
-									presenceInfo: updatedPresenceInfo,
-								});
-							}}
-						/>{" "}
-						<label htmlFor={key}>{key}</label>
-					</p>
-				))}
+				<div className="grid grid-flow-row grid-cols-2 gap-4 rounded-lg bg-gray-200 p-4">
+					{Object.entries(presenceState).map(([key, value]) => (
+						<p key={key}>
+							<input
+								type="checkbox"
+								key={key}
+								id={key}
+								checked={value}
+								onChange={() => {
+									const updatedPresenceInfo = { ...presenceState, [key]: !value };
+									setPresenceState(updatedPresenceInfo);
+									presenceMutation.mutate({
+										id: id ?? "",
+										presenceInfo: updatedPresenceInfo,
+									});
+								}}
+							/>{" "}
+							<label htmlFor={key}>{key}</label>
+						</p>
+					))}
+				</div>
 				<br />
 			</OnlyRole>
-			{...Object.keys(hackerData).map(key => (
+			<h2 className="self-center font-[Coolvetica] text-2xl font-normal text-dark">General Information</h2>
+			<p className={paragraphClass}>
+				<b className={boldClass}>Contact Information</b> {hackerData.email} &mdash; {hackerData.phoneNumber}
+			</p>
+			<p className={paragraphClass}>
+				<b className={boldClass}>Emergency Contact</b> {hackerData.emergencyContactName} (
+				{hackerData.emergencyContactRelationship}) {hackerData.emergencyContactPhoneNumber}
+			</p>
+			<p className={paragraphClass}>
+				<b className={boldClass}>Requirements</b>
+				{hackerData.dietaryRestrictions
+					? hackerData.dietaryRestrictions
+					: "No Dietary Restrictions"} &mdash;{" "}
+				{hackerData.accessibilityRequirements
+					? hackerData.accessibilityRequirements
+					: "No Accessibility Requirements"}{" "}
+				&mdash; {hackerData.preferredLanguage} &mdash; {hackerData.shirtSize}
+			</p>
+			<p className={paragraphClass}>
+				<b className={boldClass}>Attendance</b>
+				{hackerData.confirmed ? "Confirmed attendance" : "Unconfirmed attendance"} &mdash;{" "}
+				{hackerData.unsubscribed ? "Unsubscribed from emails" : "Subscribed to emails"} &mdash; Attends{" "}
+				{hackerData.attendanceType} at {hackerData.attendanceLocation} with{" "}
+				{hackerData.transportationRequired ? "" : "no"} transportation required
+			</p>
+			<p className="flex flex-row flex-wrap gap-4">
+				{Object.entries({
+					Resume: hackerData.linkResume,
+					LinkedIn: hackerData.linkLinkedin,
+					GitHub: hackerData.linkGithub,
+					"Personal Website": hackerData.linkPersonalSite,
+				}).map(
+					([key, value]) =>
+						value && (
+							<a
+								key={key}
+								href={value}
+								target="_blank"
+								rel="noreferrer"
+								className="flex items-center justify-center gap-2 rounded-md bg-gray-800 px-4 py-2 text-white hover:bg-gray-700"
+							>
+								{key}
+							</a>
+						),
+				)}
+			</p>
+			<OnlyRole roles={[Role.ORGANIZER]}>
+				<>
+					<hr />
+					<br />
+					<h2 className="self-center font-[Coolvetica] text-2xl font-normal text-dark">Debug Information</h2>
+					<p className={paragraphClass}>
+						<b className={boldClass}>HackerInfo ID</b> {hackerData.id ?? "NULL"}
+					</p>
+					<p className={paragraphClass}>
+						<b className={boldClass}>User ID</b> {hackerData.userId ?? "NULL"}
+					</p>
+					<p className={paragraphClass}>
+						<b className={boldClass}>PresenceInfo ID</b> {hackerData.presenceInfoId ?? "NULL"}
+					</p>
+					<p className={paragraphClass}>
+						<b className={boldClass}>Unsubscribe Token</b> {hackerData.unsubscribeToken ?? "NULL"}
+					</p>
+					<p className={paragraphClass}>
+						<b className={boldClass}>Acceptance Expiry</b>{" "}
+						{(hackerData.acceptanceExpiry ?? "NULL").toString()}
+					</p>
+				</>
+			</OnlyRole>
+			{/* {...Object.keys(hackerData).map(key => (
 				<div key={key}>
 					<b>{key}</b>: {(hackerData[key as keyof HackerInfo] ?? "NULL").toString()}
 				</div>
-			))}
+			))} */}
 		</div>
 	);
 };
