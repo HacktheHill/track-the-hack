153e6a0bba1beebfb3bea095b0a5b229b1f531b4
Daniel Thorp <arcanistzed@gmail.com>
2023-02-24 01:01:18 -0500

    fix: small changes to resources and schedule
    Signed-off-by: Daniel Thorp <arcanistzed@gmail.com>

--- src/pages/schedule/index.tsx
+++ src/pages/schedule/index.tsx
@@ -60,61 +60,63 @@ const Schedule: NextPage = () => {
 			integrated={true}
 		>
 			<Tabs tab={tab} setTab={setTab} />
-			<div className="flex h-full w-full max-w-4xl flex-col gap-4 overflow-y-auto p-4 mobile:px-0">
-				{query.data
-					?.filter(event => eventTypes[event.type] === tab || tab === eventTypes.ALL)
-					.sort((a, b) => {
-						if (a.start.getTime() === b.start.getTime()) {
-							return a.end.getTime() - b.end.getTime();
-						}
-						return a.start.getTime() - b.start.getTime();
-					})
-					.reduce((acc, event, i, array) => {
-						if (array[i]?.start.getDate() === array[i - 1]?.start.getDate()) {
-							acc[acc.length - 1]?.push(event);
-						} else {
-							acc.push([event]);
-						}
-						return acc;
-					}, [] as Event[][])
-					.map((event, i) => (
-						<div key={i} className="flex gap-4">
-							<div className="grid basis-1/3 place-content-center rounded-xl bg-dark/50 p-4 font-coolvetica text-2xl text-white">
-								{event[0]?.start.toLocaleDateString(dateLocale, {
-									month: "short",
-									day: "numeric",
-								})}
+			<div className="w-full overflow-y-auto p-4 mobile:px-0">
+				<div className="mx-auto flex max-w-2xl flex-col gap-4">
+					{query.data
+						?.filter(event => eventTypes[event.type] === tab || tab === eventTypes.ALL)
+						.sort((a, b) => {
+							if (a.start.getTime() === b.start.getTime()) {
+								return a.end.getTime() - b.end.getTime();
+							}
+							return a.start.getTime() - b.start.getTime();
+						})
+						.reduce((acc, event, i, array) => {
+							if (array[i]?.start.getDate() === array[i - 1]?.start.getDate()) {
+								acc[acc.length - 1]?.push(event);
+							} else {
+								acc.push([event]);
+							}
+							return acc;
+						}, [] as Event[][])
+						.map((event, i) => (
+							<div key={i} className="flex gap-4">
+								<div className="grid basis-1/3 place-content-center rounded-xl bg-dark/50 p-4 font-coolvetica text-2xl text-white">
+									{event[0]?.start.toLocaleDateString(dateLocale, {
+										month: "short",
+										day: "numeric",
+									})}
+								</div>
+								<div className="flex w-full flex-col gap-4">
+									{event.map(event => {
+										index++;
+										return (
+											<Link
+												key={event.id}
+												href={`/schedule/event?id=${event.id}`}
+												className={`flex flex-col items-center justify-center gap-2 rounded-xl p-3 font-coolvetica text-dark ${
+													index % 2 === 0 ? "bg-accent1" : "bg-accent2"
+												}`}
+											>
+												<h1 className="text-center text-xl">{event.name}</h1>
+												<p className="text-center leading-3">
+													{event.start.toLocaleTimeString(dateLocale, {
+														hour: "numeric",
+														minute: "numeric",
+													})}
+													{" - "}
+													{event.end.toLocaleTimeString(dateLocale, {
+														hour: "numeric",
+														minute: "numeric",
+													})}
+												</p>
+												<p>{event.room}</p>
+											</Link>
+										);
+									})}
+								</div>
 							</div>
-							<div className="flex w-full flex-col gap-4">
-								{event.map(event => {
-									index++;
-									return (
-										<Link
-											key={event.id}
-											href={`/schedule/event?id=${event.id}`}
-											className={`flex flex-col items-center justify-center gap-2 rounded-xl p-3 font-coolvetica text-dark ${
-												index % 2 === 0 ? "bg-accent1" : "bg-accent2"
-											}`}
-										>
-											<h1 className="text-center text-xl">{event.name}</h1>
-											<p className="text-center leading-3">
-												{event.start.toLocaleTimeString(dateLocale, {
-													hour: "numeric",
-													minute: "numeric",
-												})}
-												{" - "}
-												{event.end.toLocaleTimeString(dateLocale, {
-													hour: "numeric",
-													minute: "numeric",
-												})}
-											</p>
-											<p>{event.room}</p>
-										</Link>
-									);
-								})}
-							</div>
-						</div>
-					))}
+						))}
+				</div>
 			</div>
 		</App>
 	);
@@ -128,7 +130,7 @@ type TabsProps = {
 const Tabs = ({ tab, setTab }: TabsProps) => {
 	return (
 		<div className="sticky top-0 w-full border-b border-dark bg-background1 from-background2 px-4 pt-2 pb-4 shadow-navbar">
-			<div className="mx-auto grid max-w-4xl grid-cols-3 gap-3 sm:grid-cols-5">
+			<div className="mx-auto grid max-w-2xl grid-cols-3 gap-3 sm:grid-cols-5">
 				{[...new Set([eventTypes.ALL, ...Object.values(eventTypes)])].map(name => (
 					<Tab key={name} name={name} active={tab} onClick={() => setTab(name)} />
 				))}
