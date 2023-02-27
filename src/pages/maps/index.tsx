37fa414fc2062ef3043210ad868b60609ae10d3a
omersayshi <omerabubakerca@gmail.com>
2023-02-27 00:50:37 -0800

    feat: maps Component (#58)
    - removed notif component
    - added maps component
    
    whats left:
    - maps mobile logo
    - design sign off

---
+++ src/pages/maps/index.tsx
@@ -0,0 +1,45 @@
+import type { GetStaticProps } from "next";
+import { useTranslation } from "next-i18next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
+import App from "../../components/App";
+import Image from "next/image";
+
+export const getStaticProps: GetStaticProps = async ({ locale }) => {
+	return {
+		props: await serverSideTranslations(locale ?? "en", ["common", "qr"]),
+	};
+};
+
+const Maps = () => {
+	const { t } = useTranslation("qr");
+	const mapsLink = (floor: number) => `https://cdn1.hackthehill.com/crx/floorplan/${floor}.png`;
+	const MAX_FLOORS = 6;
+
+	return (
+		<App className="flex h-0 flex-col items-center bg-gradient-to-b from-background2 to-background1">
+			<div className="w-full justify-center overflow-y-auto mobile:px-0">
+				{
+					// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+					[...Array(MAX_FLOORS)].map((e, i) => (
+						<>
+							<h1 className="py-3 text-center text-xl text-dark">Floor {i}</h1>
+
+							<TransformWrapper>
+								<div className="mx-auto block">
+									<TransformComponent
+										wrapperStyle={{ display: "block", marginLeft: "auto", marginRight: "auto" }}
+									>
+										<Image width={800} height={400} src={mapsLink(i)} alt={`Floor ${i}`} />
+									</TransformComponent>
+								</div>
+							</TransformWrapper>
+						</>
+					))
+				}
+			</div>
+		</App>
+	);
+};
+
+export default Maps;
