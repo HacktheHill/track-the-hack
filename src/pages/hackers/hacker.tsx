1d6d978ffa81ef270e741ec095aa0ed2f4afffbe
Emilien Breton <bricktech2000@gmail.com>
2023-02-28 14:50:02 -0500

    feat: improve labels on `hackers` page

--- src/pages/hackers/hacker.tsx
+++ src/pages/hackers/hacker.tsx
@@ -104,6 +104,17 @@ const HackerView = ({ hackerData, presenceData: { id: _, hackerInfoId, ...presen
 	const paragraphClass = "flex justify-between gap-4 text-right";
 	const boldClass = "text-left font-bold";
 
+	const keyToLabel: Record<keyof PresenceInfo, string> = {
+		id: "",
+		hackerInfoId: "",
+		checkedIn: "Checked In",
+		breakfast1: "Breakfast March 3rd",
+		lunch1: "Lunch March 3rd",
+		dinner1: "Dinner March 3rd",
+		breakfast2: "Breakfast March 4th",
+		lunch2: "Lunch March 4th",
+	};
+
 	return (
 		<>
 			<h1 className="self-center font-coolvetica text-4xl font-normal text-dark">
@@ -132,7 +143,7 @@ const HackerView = ({ hackerData, presenceData: { id: _, hackerInfoId, ...presen
 									});
 								}}
 							/>{" "}
-							<label htmlFor={key}>{key}</label>
+							<label htmlFor={key}>{keyToLabel[key as keyof PresenceInfo]}</label>
 						</p>
 					))}
 				</div>
