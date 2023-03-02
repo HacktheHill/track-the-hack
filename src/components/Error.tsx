7fb220fa4b54de48c65014b5f980243e108cc83d
Daniel Thorp <arcanistzed@gmail.com>
2023-03-01 22:47:41 -0500

    chore: use Link component in Error
    Signed-off-by: Daniel Thorp <arcanistzed@gmail.com>

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -1,4 +1,5 @@
 import { useTranslation } from "next-i18next";
+import Link from "next/link";
 
 type ErrorProps = {
 	message: string;
@@ -13,14 +14,14 @@ const Error = ({ message }: ErrorProps) => {
 					message,
 				})}
 			</code>
-			<a href="mailto:development@hackthehill.com" target="_blank" rel="noreferrer">
+			<Link href="mailto:development@hackthehill.com" target="_blank" rel="noreferrer">
 				<button
 					type="button"
 					className="transform cursor-pointer whitespace-nowrap rounded-normal border-2 border-light px-[calc(2*clamp(.75rem,1vmin,5rem))] py-[clamp(0.75rem,1vmin,5rem)] font-rubik text-[clamp(1rem,1vmin,5rem)] text-light shadow-[0_15px_25px_rgba(0,_0,_0,_0.15),_0_5px_10px_rgba(0,_0,_0,_0.05)] transition hover:bg-white/50"
 				>
 					{t("contact-us")}
 				</button>
-			</a>
+			</Link>
 		</>
 	);
 };
