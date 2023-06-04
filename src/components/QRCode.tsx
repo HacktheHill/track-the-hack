be316888f8c5f8ab2cc6139843d0f5fa3fa75dd2
Osa <85007593+osaaa@users.noreply.github.com>
2023-06-04 12:45:41 -0400

    possible fix to the bug

--- src/components/QRCode.tsx
+++ src/components/QRCode.tsx
@@ -6,6 +6,7 @@ import { useEffect, useState } from "react";
 import { trpc } from "../utils/api";
 
 import Error from "./Error";
+import router from "next/router";
 
 type QRCodeProps = {
 	setError?: (error: boolean) => void;
@@ -45,6 +46,21 @@ const QRCode = ({ setError, id }: QRCodeProps) => {
 		}
 		void getQRCode();
 	}, [id, query.data]);
+
+	useEffect(() => {
+		// Check if user is unregistered and redirect if necessary
+		const redirectUser = async () => {
+			if (!query.data) {
+				try {
+					await router.push("/"); // Replace "/" with the desired redirection path
+				} catch (error) {
+					console.error(error);
+				}
+			}
+		};
+
+		void redirectUser();
+	}, [query.data]);
 
 	if (query.isError) {
 		setError?.(true);
