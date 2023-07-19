ea6ef003a4e4413c30613aa633dc275afebf09c5
ghoulie-jpg <104750700+ghoulie-jpg@users.noreply.github.com>
2023-07-18 23:22:22 -0400

    feat: replaced useEffect redirects (#85)
    Same pull request as before but with git module removed.
    
    ---------
    
    Signed-off-by: Daniel Thorp <arcanistzed@gmail.com>
    Co-authored-by: macsaheen <saheen_j@ymail.com>
    Co-authored-by: Daniel Thorp <arcanistzed@gmail.com>

---
+++ src/utils/redirects.ts
@@ -0,0 +1,48 @@
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { PrismaClient } from "@prisma/client";
+import type { Session } from "next-auth";
+import type { SSRConfig } from "next-i18next";
+
+type ServerSideProps = {
+	redirect?: {
+		destination: string;
+		permanent: boolean;
+	};
+	props: SSRConfig;
+};
+
+export async function hackersRedirect(session: Session | null, locale: string | undefined): Promise<ServerSideProps> {
+	const prisma = new PrismaClient();
+
+	const user =
+		session &&
+		(await prisma.user.findUnique({
+			where: {
+				id: session.user?.id,
+			},
+		}));
+
+	if (!user) {
+		return {
+			redirect: {
+				destination: "/api/auth/signin",
+				permanent: false,
+			},
+			props: {},
+		};
+	}
+
+	if (user.role !== "ORGANIZER") {
+		return {
+			redirect: {
+				destination: "/",
+				permanent: false,
+			},
+			props: {},
+		};
+	}
+
+	return {
+		props: await serverSideTranslations(locale ?? "en", ["common", "hackers"]),
+	};
+}
