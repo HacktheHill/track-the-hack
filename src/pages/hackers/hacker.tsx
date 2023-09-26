5c408483c6ea60ba574232d50ac8e377bc89a916
ϻartin <51332188+martin0024@users.noreply.github.com>
2023-09-26 00:12:05 -0400

    feat: update hacker details page.

--- src/pages/hackers/hacker.tsx
+++ src/pages/hackers/hacker.tsx
@@ -10,6 +10,7 @@ import App from "../../components/App";
 import Error from "../../components/Error";
 import Loading from "../../components/Loading";
 import OnlyRole from "../../components/OnlyRole";
+import Link from "next/link";
 
 type HackerInfo = Prisma.HackerInfoGetPayload<true>;
 type PresenceInfo = Prisma.PresenceInfoGetPayload<true>;
@@ -94,7 +95,6 @@ type HackerViewProps = {
 	hackerData: HackerInfo;
 	presenceData: PresenceInfo;
 };
-
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const HackerView = ({ hackerData, presenceData: { id: _, hackerInfoId, ...presenceData } }: HackerViewProps) => {
 	const router = useRouter();
@@ -103,9 +103,10 @@ const HackerView = ({ hackerData, presenceData: { id: _, hackerInfoId, ...presen
 	const presenceMutation = trpc.presence.update.useMutation();
 	const [presenceState, setPresenceState] = useState(presenceData);
 
-	const paragraphClass = "flex justify-between gap-4 text-right";
+	const paragraphClass = "flex justify-between gap-4 text-right ";
 	const boldClass = "text-left font-bold";
 
+	const [edit, setEdit] = useState(false);
 	const keyToLabel = {
 		checkedIn: "Checked In",
 		breakfast1: "Breakfast March 4th",
@@ -125,7 +126,7 @@ const HackerView = ({ hackerData, presenceData: { id: _, hackerInfoId, ...presen
 				{hackerData.firstName} {hackerData.lastName} ({hackerData.gender})
 			</h1>
 			<p className="self-center">
-				{hackerData.studyLevel} {hackerData.studyProgram}{" "}
+				{hackerData.studyLevel?.toUpperCase()} {hackerData.studyProgram}{" "}
 				{hackerData.university && `at ${hackerData.university}`}{" "}
 				{hackerData.graduationYear && `(${hackerData.graduationYear})`}
 			</p>
@@ -134,6 +135,7 @@ const HackerView = ({ hackerData, presenceData: { id: _, hackerInfoId, ...presen
 					{Object.entries(presenceState).map(([key, value]) => (
 						<p key={key}>
 							<input
+								className="form-checkbox accent-[#3b4779] h-3.5 w-5 text-gray-800 rounded "
 								type="checkbox"
 								key={key}
 								id={key}
@@ -152,71 +154,162 @@ const HackerView = ({ hackerData, presenceData: { id: _, hackerInfoId, ...presen
 					))}
 				</div>
 			</OnlyRole>
-			<h2 className="self-center font-[Coolvetica] text-2xl font-normal text-dark">General Information</h2>
-			<p className={paragraphClass}>
-				<b className={boldClass}>Contact Information</b> {hackerData.email} &mdash; {hackerData.phoneNumber}
-			</p>
-			<p className={paragraphClass}>
-				<b className={boldClass}>Emergency Contact</b> {hackerData.emergencyContactName} (
-				{hackerData.emergencyContactRelationship}) {hackerData.emergencyContactPhoneNumber}
-			</p>
-			<p className={paragraphClass}>
-				<b className={boldClass}>Requirements</b>
-				{hackerData.dietaryRestrictions
-					? hackerData.dietaryRestrictions
-					: "No Dietary Restrictions"} &mdash;{" "}
-				{hackerData.accessibilityRequirements
-					? hackerData.accessibilityRequirements
-					: "No Accessibility Requirements"}{" "}
-				&mdash; {hackerData.preferredLanguage} &mdash; {hackerData.shirtSize}
-			</p>
-			<p className={paragraphClass}>
-				<b className={boldClass}>Attendance</b>
-				{hackerData.confirmed ? "Confirmed attendance" : "Unconfirmed attendance"}
-				{hackerData.walkIn ? " (walk-in)" : ""} &mdash;{" "}
-				{hackerData.unsubscribed ? "Unsubscribed from emails" : "Subscribed to emails"} &mdash; Attends{" "}
-				{hackerData.attendanceType} at {hackerData.location} with{" "}
-				{hackerData.transportationRequired ? "" : "no"} transportation required
-			</p>
-			<p className="flex flex-row flex-wrap gap-4">
-				{Object.entries({
-					Resume: hackerData.linkResume,
-					LinkedIn: hackerData.linkLinkedin,
-					GitHub: hackerData.linkGithub,
-					"Personal Website": hackerData.linkPersonalSite,
-				}).map(
-					([key, value]) =>
-						value && (
-							<a
-								key={key}
-								href={value}
-								target="_blank"
-								rel="noreferrer"
-								className="flex items-center justify-center gap-2 rounded-md bg-gray-800 px-4 py-2 text-white hover:bg-gray-700"
+
+			<div className="flex justify-center ">
+						<h2 className="self-center font-[Coolvetica] text-2xl font-normal text-dark px-3 py">{edit ? "Edit Informations" : "Personnal Information"}</h2>
+						<Link href={`/edit?id=${hackerData.id}`}>
+							<button className="text-sm px-3 py-1 bg-gray-800 rounded text-white hover:bg-gray-700">Edit</button>
+						</Link>
+			</div>
+
+
+				<div>
+					<p className={paragraphClass}>
+						<b className={boldClass}>Email Address</b> {hackerData.gender}
+					</p>
+
+					<p className={paragraphClass}>
+						<b className={boldClass}>First Name</b> {hackerData.firstName}
+					</p>
+					<p className={paragraphClass}>
+						<b className={boldClass}>Last Name</b> {hackerData.lastName}
+					</p>
+
+					<p className={paragraphClass}>
+						<b className={boldClass}>University</b> {hackerData.university}
+					</p>
+					<p className={paragraphClass}>
+						<b className={boldClass}>Study Level</b> {hackerData.studyLevel?.toUpperCase()}
+					</p>
+					<p className={paragraphClass}>
+						<b className={boldClass}>Study Program</b> {hackerData.studyProgram}
+					</p>
+					<p className={paragraphClass}>
+						<b className={boldClass}>Graduation Year</b> {hackerData.graduationYear}
+					</p>
+
+					<div className="flex justify-center">
+						<h2 className="self-center font-[Coolvetica] text-2xl font-normal text-dark px-3 py">Emergency Contact</h2>
+						{!edit && (
+							<button
+								onClick={() => setEdit(true)}
+								className="text-sm px-3 py-1 bg-gray-800 rounded text-white hover:bg-gray-700"
 							>
-								{key}
-							</a>
-						),
-				)}
-			</p>
-			<OnlyRole filter={role => role === Role.ORGANIZER}>
-				<>
-					<h2 className="self-center font-[Coolvetica] text-2xl font-normal text-dark">Debug Information</h2>
+								Edit
+							</button>
+						)}
+					</div>
+
 					<p className={paragraphClass}>
-						<b className={boldClass}>HackerInfo ID</b> {hackerData.id ?? "NULL"}
+						<b className={boldClass}>Contact Name</b> {hackerData.emergencyContactName}
 					</p>
 					<p className={paragraphClass}>
-						<b className={boldClass}>User ID</b> {hackerData.userId ?? "NULL"}
+						<b className={boldClass}>Contact Relationship</b> {hackerData.emergencyContactRelationship}
 					</p>
 					<p className={paragraphClass}>
-						<b className={boldClass}>Unsubscribe Token</b> {hackerData.unsubscribeToken ?? "NULL"}
+						<b className={boldClass}>Contact Phone Number</b> {hackerData.emergencyContactPhoneNumber}
+					</p>
+
+					<div className="flex justify-center">
+						<h2 className="self-center font-[Coolvetica] text-2xl font-normal text-dark px-3 py">General Information</h2>
+						{!edit && (
+							<button
+								onClick={() => setEdit(true)}
+								className="text-sm px-3 py-1 bg-gray-800 rounded text-white hover:bg-gray-700"
+							>
+								Edit
+							</button>
+						)}
+					</div>
+
+					<p className={paragraphClass}>
+						<b className={boldClass}>Dietary Restrictions</b>{" "}
+						{hackerData.dietaryRestrictions !== "None" ? hackerData.dietaryRestrictions : "No Dietary Restrictions"}
+					</p>
+
+					<p className={paragraphClass}>
+						<b className={boldClass}>Accessibility Requirements</b>{" "}
+						{hackerData.accessibilityRequirements !== "None"
+							? hackerData.accessibilityRequirements
+							: "No Accessibility Requirements"}
+					</p>
+
+					<p className={paragraphClass}>
+						<b className={boldClass}>Preferred Language</b>
+						{hackerData.preferredLanguage}
 					</p>
 					<p className={paragraphClass}>
-						<b className={boldClass}>Acceptance Expiry</b>{" "}
-						{(hackerData.acceptanceExpiry ?? "NULL").toString()}
+						<b className={boldClass}>Shirt Size</b>
+						{hackerData.shirtSize}
 					</p>
-				</>
-			</OnlyRole>
+
+					<p className={paragraphClass}>
+						<b className={boldClass}>Confirmed</b>
+						{hackerData.confirmed ? "Confirmed attendance" : "Unconfirmed attendance"}
+					</p>
+
+					<p className={paragraphClass}>
+						<b className={boldClass}>Walk In</b>
+						{hackerData.walkIn ? " Walk-In" : ""}
+					</p>
+					<p className={paragraphClass}>
+						<b className={boldClass}>Subscribed</b>{" "}
+						{hackerData.unsubscribed ? "Unsubscribed from emails" : "Subscribed to emails"}
+					</p>
+					<p className={paragraphClass}>
+						<b className={boldClass}>AttendanceType</b>
+						{hackerData.attendanceType}
+					</p>
+					<p className={paragraphClass}>
+						<b className={boldClass}>Location</b>
+						{hackerData.location}
+					</p>
+					<p className={paragraphClass}>
+						<b className={boldClass}>Transportation Required</b>{" "}
+						{hackerData.transportationRequired ? "Transportation Required" : "No Transportation Required"}
+					</p>
+
+					<p className="flex flex-row flex-wrap gap-4 justify-center">
+						{Object.entries({
+							Resume: hackerData.linkResume,
+							LinkedIn: hackerData.linkLinkedin,
+							GitHub: hackerData.linkGithub,
+							"Personal Website": hackerData.linkPersonalSite,
+						}).map(([key, value]) =>
+								value && (
+									<a
+										key={key}
+										href={value}
+										target="_blank"
+										rel="noreferrer"
+										className="flex items-center justify-center gap-2 rounded-md bg-dark px-4 py-2 text-white hover:bg-gray-700"
+									>
+										{key}
+									</a>
+								)
+						)}
+					</p>
+
+
+					<OnlyRole filter={role => role === Role.ORGANIZER}>
+						<>
+							<h2 className="self-center font-[Coolvetica] text-2xl font-normal text-dark">Debug Information</h2>
+							<p className={paragraphClass}>
+								<b className={boldClass}>HackerInfo ID</b> {hackerData.id ?? "NULL"}
+							</p>
+							<p className={paragraphClass}>
+								<b className={boldClass}>User ID</b> {hackerData.userId ?? "NULL"}
+							</p>
+							<p className={paragraphClass}>
+								<b className={boldClass}>Unsubscribe Token</b> {hackerData.unsubscribeToken ?? "NULL"}
+							</p>
+							<p className={paragraphClass}>
+								<b className={boldClass}>Acceptance Expiry</b>{" "}
+								{(hackerData.acceptanceExpiry ?? "NULL").toString()}
+							</p>
+						</>
+					</OnlyRole>
+				</div>
 		</>
 	);
 };
