570583688bd88cd95b3f1cab4f7598b2283d3e7d
Daniel Thorp <arcanistzed@gmail.com>
2023-09-27 02:03:48 -0400

    fix: remove label and button
    Signed-off-by: Daniel Thorp <arcanistzed@gmail.com>

--- src/components/PhysicalScanner.tsx
+++ src/components/PhysicalScanner.tsx
@@ -18,29 +18,16 @@ const PhysicalScanner = (props: PhysicalScannerProps) => {
 	};
 
 	return (
-		<>
-			<form onSubmit={onSubmit}>
-				<label htmlFor="scannerInput" className="flex-[50%] font-rubik text-dark">
-					Physical Scanner
-				</label>
-				<input
-					name="scannerInput"
-					type="text"
-					autoFocus
-					className="w-full rounded-[100px] border-none bg-background1 px-4 py-2 font-rubik text-dark shadow-md transition-all duration-500 hover:bg-background1/50"
-					onChange={onChange}
-					value={scannedCode}
-				/>
-			</form>
-			{scannedCode && (
-				<button
-					onClick={() => setScannedCode("")}
-					className="cursor-pointer whitespace-nowrap rounded-[100px] border-none bg-light px-8 py-2 font-rubik text-white shadow-md transition-all duration-1000 hover:bg-medium"
-				>
-					Clear Physical
-				</button>
-			)}
-		</>
+		<form onSubmit={onSubmit}>
+			<input
+				name="scannerInput"
+				type="text"
+				autoFocus
+				className="w-full rounded-[100px] border-none bg-background1 px-4 py-2 font-rubik text-dark shadow-md transition-all duration-500 hover:bg-background1/50"
+				onChange={onChange}
+				value={scannedCode}
+			/>
+		</form>
 	);
 };
 
