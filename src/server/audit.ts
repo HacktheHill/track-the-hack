b9cc42637dbf1255b448769ba7541284ddcd9643
ϻartin <51332188+martin0024@users.noreply.github.com>
2023-10-06 19:02:28 -0400

    feat: add audit.ts

---
+++ src/server/audit.ts
@@ -0,0 +1,31 @@
+// utils/auditUtils.ts
+
+import { PrismaClient } from '@prisma/client';
+
+async function logAuditEntry(
+    ctx: {
+        prisma: PrismaClient;
+    },
+    userId: string,
+    route: string,
+    action: string,
+    author: string,
+    details: string
+): Promise<void> {
+    try {
+        await ctx.prisma.auditLog.create({
+            data: {
+                timestamp: new Date(),
+                user_id: userId,
+                route,
+                action,
+                author,
+                details,
+            },
+        });
+    } catch (error) {
+        console.error('Error creating audit log entry:', error);
+    }
+}
+
+export { logAuditEntry };
