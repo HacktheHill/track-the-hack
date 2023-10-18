70ef85fa54257f289bd7b153c9dbc046624f3f89
Kevin Wu <95104238+kevinwu15@users.noreply.github.com>
2023-10-18 18:43:12 -0400

    feat: next previous buttons (#103)
    Demo of changes:
    
    ![PrevNextPR](https://github.com/HacktheHill/track-the-hack/assets/95104238/6e7964eb-7b64-463c-9655-65bdc246a11d)
    
    Changes:
    Added new hacker routers to get the next and previous hacker from a
    unique id (in the url) using cursor-based pagination. Buttons are just
    preliminary and should be changed based on desired design later

--- src/pages/hackers/hacker.tsx
+++ src/pages/hackers/hacker.tsx
@@ -27,6 +27,9 @@ const Hacker: NextPage = () => {
 
 	const hackerQuery = trpc.hackers.get.useQuery({ id: id ?? "" }, { enabled: !!id });
 	const presenceQuery = trpc.presence.getFromHackerId.useQuery({ id: id ?? "" }, { enabled: !!id });
+	
+	const nextHackerQuery = trpc.hackers.getNext.useQuery({ id: id ?? "" }, { enabled: !!id });
+	const prevHackerQuery = trpc.hackers.getPrev.useQuery({ id: id ?? "" }, { enabled: !!id });
 
 	if (hackerQuery.isLoading || hackerQuery.data == null) {
 		return (
@@ -82,6 +85,24 @@ const Hacker: NextPage = () => {
 		>
 			<div className="mx-auto flex max-w-2xl flex-col gap-4">
 				<OnlyRole filter={role => role === Role.ORGANIZER || role === Role.SPONSOR}>
+					<div className="flex justify-between">
+						{prevHackerQuery.data ? (<a
+							href={`/hackers/hacker?id=${prevHackerQuery.data.id}`}
+							className="flex items-center justify-center gap-2 rounded-md bg-gray-800 px-4 py-2 text-white hover:bg-gray-700"
+						>
+							Previous
+						</a>
+						) : (
+							<a></a>
+						)}
+						{nextHackerQuery.data && ( <a
+							href={`/hackers/hacker?id=${nextHackerQuery.data.id}`}
+							className="flex items-center justify-center gap-2 rounded-md bg-gray-800 px-4 py-2 text-white hover:bg-gray-700"
+						>
+							Next
+						</a>
+						)}
+					</div>
 					<HackerView hackerData={hackerQuery.data} presenceData={presenceQuery.data} />
 				</OnlyRole>
 				<OnlyRole filter={role => role === Role.HACKER}>{t("not-authorized-to-view-this-page")}</OnlyRole>
