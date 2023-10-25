91561afca8a1f2c148f8110ca41c14104ff1e43e
amybradbrook <142829358+amybradbrook@users.noreply.github.com>
2023-10-25 18:53:03 -0400

    Add files via upload

---
+++ src/pages/qr/popup.tsx
@@ -0,0 +1,137 @@
+import { Role, type Prisma } from "@prisma/client";
+import { useTranslation } from "next-i18next";
+import { useState } from "react";
+import Image from "next/image";
+import type { GetServerSideProps } from "next";
+import { getServerSession } from "next-auth/next";
+import { hackersRedirect } from "../../utils/redirects";
+import { authOptions } from "../api/auth/[...nextauth]";
+import { trpc } from "../../utils/api";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
+const Popup = ({ id, selectedOption }) => {
+
+	const router = useRouter()
+    const presenceQuery = trpc.presence.getFromHackerId.useQuery({id: id ?? ""}, {enabled: !!id})
+	const hackerQuery = trpc.hackers.get.useQuery({id: id ?? ""}, {enabled: !!id})
+	const presenceMutation = trpc.presence.update.useMutation();
+
+	const [firstName, setFirstName]=useState("")
+	const [lastName, setLastName]=useState("")
+	const [shirt, setShirt]=useState("")
+	const [visibility, setVisibility] = useState("invisible");
+	const [queriesLoaded, setQueriesLoaded]=useState(false)
+    
+	useEffect(()=>{
+		if (!hackerQuery.isLoading && !presenceQuery.isLoading){
+			if (hackerQuery.data){
+				setFirstName(hackerQuery.data.firstName)
+				setLastName(hackerQuery.data.lastName)
+				setShirt(hackerQuery.data.shirtSize)	
+			} 
+			if (presenceQuery.data){
+				if (presenceQuery.data[selectedOption]===false){
+					setVisibility('bg-[#7cf77c] h-30 w-96 z-30 text-center p-2 rounded text-lg')
+					const updatedPresenceInfo = { ...presenceQuery.data, [selectedOption]: true}
+					console.log(updatedPresenceInfo)
+					presenceMutation.mutate({
+						id: id ?? "",
+						presenceInfo: updatedPresenceInfo
+					})
+				} else {
+					setVisibility('bg-[#FF0000] h-30 w-96 z-30 text-center p-2 rounded text-lg')
+				}
+			}
+		}
+
+	}, [hackerQuery.isLoading, presenceQuery.isLoading, hackerQuery.data, presenceQuery.data]);
+
+	useEffect(()=>{
+		if (firstName!=="" && lastName!=="" && shirt!=="" && visibility!=="invisible"){
+			setQueriesLoaded(true)
+		}
+	},[firstName, lastName, shirt])
+
+	return(
+        <div>
+			{queriesLoaded && <div>
+				{selectedOption==="checkedIn" && (
+					<div className={visibility}>
+						<h6 className="font-bold">{firstName} {lastName}</h6>
+						<h6>Shirt Size: {shirt}</h6>
+						<button onClick={()=>{void router.push(`/hackers/hacker?id=${id}`)}}></button>
+					</div>
+				)}
+				{selectedOption==="breakfast1" && (
+					<div className={visibility}>
+						<h6 className="font-bold">{firstName} {lastName}</h6>
+					</div>
+				)}
+				{selectedOption==="lunch1" && (
+					<div className={visibility}>
+						<h6 className="font-bold">{firstName} {lastName}</h6>
+					</div>
+				)}
+				{selectedOption==="dinner1" && (
+					<div className={visibility}>
+						<h6 className="font-bold">{firstName} {lastName}</h6>
+					</div>
+				)}
+				{selectedOption==="breakfast2" && (
+					<div className={visibility}>
+						<h6 className="font-bold">{firstName} {lastName}</h6>
+					</div>
+				)}
+				{selectedOption==="lunch2" && (
+					<div className={visibility}>
+						<h6 className="font-bold">{firstName} {lastName}</h6>
+					</div>
+				)}					
+			</div>}
+        </div>
+        
+    )
+}
+
+export default Popup
+
+							//eventSignInValue works, but it's not true or false!! We need a prisma check here to see if the value is true or false!
+							// if (eventSignInValueElement) {
+							// 	console.log(eventSignInValueElement)
+								// if (eventSignInValueElement==false){
+								// 	//Its not going to here
+								// 	console.log("scanned")
+								// 	setVisibility('bg-[#7cf77c] h-30 w-96 z-30 text-center p-2 rounded text-lg')
+								// 	const presenceMutation = trpc.presence.update.useMutation();
+								// 	const updatedPresenceInfo = {
+								// 		...presenceState, 
+								// 		eventSignInValueElement: true
+								// 	}
+								// 	setPresenceState(updatedPresenceInfo);
+								// 	presenceMutation.mutate({
+								// 		id: data ??"",
+								// 		presenceInfo: updatedPresenceInfo
+								// 	})
+								// 	if (selectedOption==='OpeningCeremony'){
+								// 		const hackerQuery = trpc.hackers.get.useQuery({id: data ?? ""}, {enabled:!!data})
+								// 		const firstName=hackerQuery.data.firstName
+								// 		const lastName=hackerQuery.data.lastName
+								// 		const fullName = firstName.concat(lastName)
+								// 		setName(fullName)
+		
+								// 		const shirtSize=hackerQuery.data.shirtSize
+								// 		setShirt(shirtSize)
+								// 	}
+								// 	else{
+								// 		const hackerQuery = trpc.hackers.get.useQuery({id: data ?? ""}, {enabled:!!data})
+								// 		const firstName=hackerQuery.data.firstName
+								// 		const lastName=hackerQuery.data.lastName
+								// 		const fullName = firstName.concat(lastName)
+								// 		setName(fullName)
+								// 	}
+								// } else {
+								// 	setVisibility('bg-[#FF0000] h-30 w-96 z-30 text-center p-2 rounded text-lg')
+								// }
+							//}
+							//Going to need to add functions to test number of sign ins
