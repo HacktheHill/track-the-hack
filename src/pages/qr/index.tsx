de51732709aeb733491b53f7ad4e879c0b4fa95d
amybradbrook <142829358+amybradbrook@users.noreply.github.com>
2023-10-25 18:52:12 -0400

    Update index.tsx

--- src/pages/qr/index.tsx
+++ src/pages/qr/index.tsx
@@ -1,29 +1,57 @@
-import { Role } from "@prisma/client";
-import type { GetServerSideProps } from "next";
-import { getServerSession } from "next-auth/next";
+import { Role, type Prisma } from "@prisma/client";
 import { useTranslation } from "next-i18next";
-import Image from "next/image";
 import { useRouter } from "next/router";
 import { useState } from "react";
+import Image from "next/image";
+import type { GetServerSideProps } from "next";
+import { getServerSession } from "next-auth/next";
 import { hackersRedirect } from "../../utils/redirects";
 import { authOptions } from "../api/auth/[...nextauth]";
+import { trpc } from "../../utils/api";
+import { useEffect } from "react";
+import Popup from "./popup";
 
-import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import App from "../../components/App";
+import Weather from "../../components/Weather";
 import OnlyRole from "../../components/OnlyRole";
-import PhysicalScanner from "../../components/PhysicalScanner";
 import QRCode from "../../components/QRCode";
 import QRScanner from "../../components/QRScanner";
-import Weather from "../../components/Weather";
 
-const QR = () => {
+//Figure out why button isn't showing
+//Figure out trpc mutations
+//Figure out how to get the qr scan to stay scanning
+
+type HackerInfo = Prisma.HackerInfoGetPayload<true>;
+type PresenceInfo = Prisma.PresenceInfoGetPayload<true>;
+
+type HackerViewProps = {
+	hackerData: HackerInfo;
+	presenceData: PresenceInfo;
+};
+
+const QR = (presenceData) => {
 	const { t } = useTranslation("qr");
 	const router = useRouter();
 	const [error, setError] = useState(false);
+	const [selectedOption, setSelectedOption] = useState("")
+	const [showPopup, setShowPopup] = useState(false)
+	const [qrData, setQRData]=useState("")
+	const [loaded, setDataLoaded]=useState(false)
 
-	const onScan = (data: string) => {
-		void router.push(`/hackers/hacker?id=${data}`);
-	};
+
+	const onChange = () => {
+		const e = document.getElementById("eventSignIn")
+		const value = e.value
+		console.log(value)
+		return value
+	}
+
+	useEffect(()=>{
+		if (showPopup && qrData !== ""){
+			console.log(qrData)
+			setDataLoaded(true)
+		}
+	}, [showPopup, qrData])
 
 	return (
 		<App
@@ -33,8 +61,13 @@ const QR = () => {
 			<Weather count={30} type="snowflake" />
 			<div className="flex flex-col items-center gap-6">
 				<OnlyRole filter={role => role === Role.ORGANIZER}>
-					<QRScanner onScan={onScan} />
-					<PhysicalScanner onScan={onScan} />
+					<QRScanner
+						onScan={(data: string) => {
+							setShowPopup(true)
+							setQRData(data)
+							
+						}}
+					/>
 					{!error && <p className="z-10 max-w-xl text-center text-lg font-bold text-dark">{t("scan-qr")}</p>}
 				</OnlyRole>
 				<OnlyRole filter={role => role === Role.HACKER}>
@@ -44,9 +77,19 @@ const QR = () => {
 			</div>
 			{error && (
 				<div className="flex h-40 items-center justify-center text-dark">
-					<p>{"sign-in-to-access"}</p>
+					<p>You need to sign in to access the QR page.</p>
 				</div>
 			)}
+			<select id="eventSignIn" className="flex flex-col items-center bg-dark font-bold text-white text-center w-120 h-10 text-lg rounded p-2" onChange={onChange}>
+				<option value=''>Select an option</option>
+				<option value="checkedIn">Opening Ceremony</option>
+				<option value="breakfast1">Friday Dinner</option>
+				<option value="lunch1">Saturday Breakfast</option>
+				<option value="dinner1">Saturday Lunch</option>
+				<option value="breakfast2">Saturday Dinner</option>
+				<option value="lunch2">Sunday Lunch</option>
+            </select>
+			{loaded && <Popup id={qrData} selectedOption={onChange()} />}
 			<div className="h-56 w-full bg-light">
 				<Image
 					priority
@@ -63,10 +106,10 @@ const QR = () => {
 
 export const getServerSideProps: GetServerSideProps = async ({ req, res, locale }) => {
 	const session = await getServerSession(req, res, authOptions);
+	const props = await hackersRedirect(session, locale);
 
 	return {
-		...(await hackersRedirect(session)),
-		props: await serverSideTranslations(locale ?? "en", ["common", "navbar", "qr"]),
+		...props,
 	};
 };
 
