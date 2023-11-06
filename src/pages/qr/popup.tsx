1a847dbe0fdf49a2777f0e045e85172db62e0d01
amybradbrook <142829358+amybradbrook@users.noreply.github.com>
2023-11-06 11:03:58 -0500

    Update popup.tsx

--- src/pages/qr/popup.tsx
+++ src/pages/qr/popup.tsx
@@ -39,10 +39,10 @@ const Popup = ({ id, selectedOption } : popUpProps) => {
 				if (presenceQuery.data[selectedOption]===false){
 					setVisibility('bg-[#7cf77c] h-30 w-96 z-30 text-center p-2 rounded text-lg')
 					const updatedPresenceInfo = { ...presenceQuery.data, [selectedOption]: true}
-					console.log(updatedPresenceInfo)
+					console.log("the updated data is", updatedPresenceInfo)
 					presenceMutation.mutate({
 						id: id ?? "",
-						presenceInfo: {[selectedOption] : true}
+						presenceInfo: {[selectedOption]: true}
 					})
 				} else {
 					setVisibility('bg-[#FF0000] h-30 w-96 z-30 text-center p-2 rounded text-lg')
@@ -64,33 +64,38 @@ const Popup = ({ id, selectedOption } : popUpProps) => {
 				{selectedOption==="checkedIn" && (
 					<div className={visibility}>
 						<h6 className="font-bold">{firstName} {lastName}</h6>
-						<h6>Shirt Size: {shirt}</h6>
-						<button onClick={()=>{void router.push(`/hackers/hacker?id=${id}`)}}></button>
+						<h6 className="font-bold"> Shirt Size: {shirt}</h6>
+						<button className="z-13 bg-white w-1/2 rounded-xl" onClick={()=>{void router.push(`/hackers/hacker?id=${id}`)}}>Go to profile</button>
 					</div>
 				)}
 				{selectedOption==="breakfast1" && (
 					<div className={visibility}>
 						<h6 className="font-bold">{firstName} {lastName}</h6>
+						<button className="z-13 bg-white w-1/2 rounded-xl" onClick={()=>{void router.push(`/hackers/hacker?id=${id}`)}}>Go to profile</button>
 					</div>
 				)}
 				{selectedOption==="lunch1" && (
 					<div className={visibility}>
 						<h6 className="font-bold">{firstName} {lastName}</h6>
+						<button className="z-13 bg-white w-1/2 rounded-xl" onClick={()=>{void router.push(`/hackers/hacker?id=${id}`)}}>Go to profile</button>
 					</div>
 				)}
 				{selectedOption==="dinner1" && (
 					<div className={visibility}>
 						<h6 className="font-bold">{firstName} {lastName}</h6>
+						<button className="z-13 bg-white w-1/2 rounded-xl" onClick={()=>{void router.push(`/hackers/hacker?id=${id}`)}}>Go to profile</button>
 					</div>
 				)}
 				{selectedOption==="breakfast2" && (
 					<div className={visibility}>
 						<h6 className="font-bold">{firstName} {lastName}</h6>
+						<button className="z-13 bg-white w-1/2 rounded-xl" onClick={()=>{void router.push(`/hackers/hacker?id=${id}`)}}>Go to profile</button>
 					</div>
 				)}
 				{selectedOption==="lunch2" && (
 					<div className={visibility}>
 						<h6 className="font-bold">{firstName} {lastName}</h6>
+						<button className="z-13 bg-white w-1/2 rounded-xl" onClick={()=>{void router.push(`/hackers/hacker?id=${id}`)}}>Go to profile</button>
 					</div>
 				)}					
 			</div>}
@@ -101,42 +106,3 @@ const Popup = ({ id, selectedOption } : popUpProps) => {
 
 export default Popup
 
-							//eventSignInValue works, but it's not true or false!! We need a prisma check here to see if the value is true or false!
-							// if (eventSignInValueElement) {
-							// 	console.log(eventSignInValueElement)
-								// if (eventSignInValueElement==false){
-								// 	//Its not going to here
-								// 	console.log("scanned")
-								// 	setVisibility('bg-[#7cf77c] h-30 w-96 z-30 text-center p-2 rounded text-lg')
-								// 	const presenceMutation = trpc.presence.update.useMutation();
-								// 	const updatedPresenceInfo = {
-								// 		...presenceState, 
-								// 		eventSignInValueElement: true
-								// 	}
-								// 	setPresenceState(updatedPresenceInfo);
-								// 	presenceMutation.mutate({
-								// 		id: data ??"",
-								// 		presenceInfo: updatedPresenceInfo
-								// 	})
-								// 	if (selectedOption==='OpeningCeremony'){
-								// 		const hackerQuery = trpc.hackers.get.useQuery({id: data ?? ""}, {enabled:!!data})
-								// 		const firstName=hackerQuery.data.firstName
-								// 		const lastName=hackerQuery.data.lastName
-								// 		const fullName = firstName.concat(lastName)
-								// 		setName(fullName)
-		
-								// 		const shirtSize=hackerQuery.data.shirtSize
-								// 		setShirt(shirtSize)
-								// 	}
-								// 	else{
-								// 		const hackerQuery = trpc.hackers.get.useQuery({id: data ?? ""}, {enabled:!!data})
-								// 		const firstName=hackerQuery.data.firstName
-								// 		const lastName=hackerQuery.data.lastName
-								// 		const fullName = firstName.concat(lastName)
-								// 		setName(fullName)
-								// 	}
-								// } else {
-								// 	setVisibility('bg-[#FF0000] h-30 w-96 z-30 text-center p-2 rounded text-lg')
-								// }
-							//}
-							//Going to need to add functions to test number of sign ins
