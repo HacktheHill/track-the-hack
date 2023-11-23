3536424e0914c199ba59a610f8d1713d1350a30b
ϻartin <51332188+martin0024@users.noreply.github.com>
2023-11-23 00:05:22 -0500

    feat: editable profiles (#100)
    https://project.hackthehill.com/projects/track-the-hack/work_packages/46/activity
    
    ---------
    
    Co-authored-by: macsaheen <saheen_j@ymail.com>

--- src/pages/hackers/hacker.tsx
+++ src/pages/hackers/hacker.tsx
@@ -10,6 +10,8 @@ import App from "../../components/App";
 import Error from "../../components/Error";
 import Loading from "../../components/Loading";
 import OnlyRole from "../../components/OnlyRole";
+import { walkInSchema } from "../../utils/common";
+import { z } from "zod";
 
 type HackerInfo = Prisma.HackerInfoGetPayload<true>;
 type PresenceInfo = Prisma.PresenceInfoGetPayload<true>;
@@ -116,17 +118,32 @@ type HackerViewProps = {
 	presenceData: PresenceInfo;
 };
 
+type Field = {
+	label: string;
+	name: string;
+	default_value: string | null | undefined;
+	type: string;
+	category: string;
+	options?: string[];
+};
+
+interface Patterns {
+	[key: string]: string | undefined;
+}
+
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const HackerView = ({ hackerData, presenceData: { id: _, hackerInfoId, ...presenceData } }: HackerViewProps) => {
 	const router = useRouter();
 	const [id] = [router.query.id].flat();
+	const { t } = useTranslation("hacker");
 
 	const presenceMutation = trpc.presence.update.useMutation();
 	const [presenceState, setPresenceState] = useState(presenceData);
 
-	const paragraphClass = "flex justify-between gap-4 text-right";
+	const paragraphClass = "flex justify-between gap-4 text-right py-1.5 ";
 	const boldClass = "text-left font-bold";
 
+	const [edit, setEdit] = useState(false);
 	const keyToLabel = {
 		checkedIn: "Checked In",
 		breakfast1: "Breakfast March 4th",
@@ -140,13 +157,260 @@ const HackerView = ({ hackerData, presenceData: { id: _, hackerInfoId, ...presen
 		lunch22: "Lunch March 5th 2",
 	} as const satisfies Record<keyof Omit<PresenceInfo, "id" | "hackerInfoId">, string>;
 
+	const fields = [
+		{
+			label: t("gender"),
+			name: "gender",
+			default_value: hackerData.gender,
+			type: "text",
+			category: t("category_personal_information"),
+		},
+		{
+			label: t("firstName"),
+			name: "firstName",
+			default_value: hackerData.firstName,
+			type: "text",
+			category: t("category_personal_information"),
+		},
+		{
+			label: t("lastName"),
+			name: "lastName",
+			default_value: hackerData.lastName,
+			type: "text",
+			category: t("category_personal_information"),
+		},
+		{
+			label: t("university"),
+			name: "university",
+			default_value: hackerData.university,
+			type: "text",
+			category: t("category_personal_information"),
+		},
+		{
+			label: t("studyLevel"),
+			name: "studyLevel",
+			default_value: hackerData.studyLevel?.toUpperCase(),
+			type: "text",
+			category: t("category_personal_information"),
+		},
+		{
+			label: t("studyProgram"),
+			name: "studyProgram",
+			default_value: hackerData.studyProgram,
+			type: "text",
+			category: t("category_personal_information"),
+		},
+		{
+			label: t("graduationYear"),
+			name: "graduationYear",
+			default_value: hackerData.graduationYear,
+			type: "number",
+			category: t("category_personal_information"),
+		},
+		{
+			label: t("phoneNumber"),
+			name: "phoneNumber",
+			default_value: hackerData.phoneNumber,
+			type: "number",
+			category: t("category_personal_information"),
+		},
+		{
+			label: t("email"),
+			name: "email",
+			default_value: hackerData.email,
+			type: "email",
+			category: t("category_personal_information"),
+		},
+		{
+			label: t("emergencyContactName"),
+			name: "emergencyContactName",
+			default_value: hackerData.emergencyContactName,
+			type: "text",
+			category: t("category_emergency_contact"),
+		},
+		{
+			label: t("emergencyContactRelationship"),
+			name: "emergencyContactRelationship",
+			default_value: hackerData.emergencyContactRelationship,
+			type: "text",
+			category: t("category_emergency_contact"),
+		},
+		{
+			label: t("emergencyContactPhoneNumber"),
+			name: "emergencyContactPhoneNumber",
+			default_value: hackerData.emergencyContactPhoneNumber,
+			type: "number",
+			category: t("category_emergency_contact"),
+		},
+		{
+			label: t("dietaryRestrictions"),
+			name: "dietaryRestrictions",
+			default_value: hackerData.dietaryRestrictions,
+			type: "text",
+			category: t("category_general_information"),
+		},
+		{
+			label: t("accessibilityRequirements"),
+			name: "accessibilityRequirements",
+			default_value: hackerData.accessibilityRequirements,
+			type: "text",
+			category: t("category_general_information"),
+		},
+		{
+			label: t("preferredLanguage"),
+			name: "preferredLanguage",
+			default_value: hackerData.preferredLanguage,
+			type: "select",
+			options: ["EN", "FR"],
+			category: t("category_general_information"),
+		},
+		{
+			label: t("shirtSize"),
+			name: "shirtSize",
+			default_value: hackerData.shirtSize,
+			type: "select",
+			options: ["S", "M", "L", "XL", "XXL"],
+			category: t("category_general_information"),
+		},
+		{
+			label: t("walkIn"),
+			name: "walkIn",
+			default_value: hackerData.walkIn,
+			type: "select",
+			options: ["true", "false"],
+			category: t("category_general_information"),
+		},
+		{
+			label: t("subscribeToMailingList"),
+			name: "subscribed",
+			default_value: hackerData.unsubscribed,
+			type: "select",
+			options: ["true", "false"],
+			category: t("category_general_information"),
+		},
+		{
+			label: t("attendanceType"),
+			name: "attendanceType",
+			default_value: hackerData.attendanceType,
+			type: "select",
+			options: ["IN_PERSON", "ONLINE"],
+			category: t("category_general_information"),
+		},
+		{
+			label: t("location"),
+			name: "location",
+			default_value: hackerData.location,
+			type: "text",
+			category: t("category_general_information"),
+		},
+		{
+			label: t("transportationRequired"),
+			name: "transportationRequired",
+			default_value: hackerData.transportationRequired,
+			type: "select",
+			options: ["true", "false"],
+			category: t("category_general_information"),
+		},
+		{
+			label: "Linkedin",
+			name: "linkLinkedin",
+			default_value: hackerData.linkLinkedin,
+			type: "url",
+			category: t("category_links_information"),
+		},
+		{
+			label: "Github",
+			name: "linkGithub",
+			default_value: hackerData.linkGithub,
+			type: "url",
+			category: t("category_links_information"),
+		},
+	];
+	const patterns: Patterns = {
+		tel: "^\\s*(?:\\+?(\\d{1,3}))?[-. (]*(\\d{3})[-. )]*(\\d{3})[-. ]*(\\d{4})(?: *x(\\d+))?\\s*$",
+		url: undefined,
+		number: "^\\d+$",
+		email: undefined,
+		text: undefined,
+	};
+
+	const initialInputValues: Record<string, string> = {};
+	const [inputValues, setInputValues] = useState<{ [key: string]: string }>(initialInputValues);
+	const groupedData: { [key: string]: Field[] } = {};
+	const mutation = trpc.hackers.update.useMutation();
+
+	const handleInputChange = (name: string, value: string) => {
+		setInputValues(prevValues => ({
+			...prevValues,
+			[name]: value,
+		}));
+		setEdit(true);
+	};
+
+	const resetInputFields = () => {
+		setInputValues(initialInputValues);
+		setEdit(false);
+	};
+
+	fields.forEach(field => {
+		initialInputValues[field.name] = String(field.default_value) || "";
+	});
+
+	fields.forEach(item => {
+		const field = item as Field;
+		if (!groupedData[field.category]) {
+			groupedData[field.category] = [];
+		}
+		groupedData[field.category]?.push(field);
+	});
+
+	const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+		const formData = new FormData(event.currentTarget);
+		const data = Object.fromEntries(formData) as Record<string, string | number | undefined>;
+		data.id = id;
+
+		if (data.linkLinkedin !== hackerData.linkLinkedin || data.linkGithub !== hackerData.linkGithub) {
+			window.location.reload();
+		}
+
+		if (typeof data.graduationYear === "string") {
+			const parsedGraduationYear = parseInt(data.graduationYear);
+			if (!isNaN(parsedGraduationYear)) {
+				data.graduationYear = parsedGraduationYear;
+			} else {
+				data.graduationYear = undefined;
+			}
+		}
+
+		const parse = walkInSchema
+			.extend({
+				id: z.string(),
+			})
+			.safeParse(data);
+
+		if (!parse.success) {
+			console.error("Validation Error:", parse.error);
+		} else {
+			console.log("Data parsed", parse.data);
+			mutation.mutate(parse.data);
+			if (!mutation.error) {
+				event.currentTarget.reset();
+			} else {
+				console.log("Error", mutation.error.message);
+			}
+		}
+		setEdit(false);
+	};
+
 	return (
 		<>
 			<h1 className="self-center font-coolvetica text-4xl font-normal text-dark">
 				{hackerData.firstName} {hackerData.lastName} ({hackerData.gender})
 			</h1>
+
 			<p className="self-center">
-				{hackerData.studyLevel} {hackerData.studyProgram}{" "}
+				{hackerData.studyLevel?.toUpperCase()} {hackerData.studyProgram}{" "}
 				{hackerData.university && `at ${hackerData.university}`}{" "}
 				{hackerData.graduationYear && `(${hackerData.graduationYear})`}
 			</p>
@@ -155,6 +419,7 @@ const HackerView = ({ hackerData, presenceData: { id: _, hackerInfoId, ...presen
 					{Object.entries(presenceState).map(([key, value]) => (
 						<p key={key}>
 							<input
+								className="form-checkbox h-3.5 w-5 rounded text-gray-800 accent-[#3b4779] "
 								type="checkbox"
 								key={key}
 								id={key}
@@ -173,71 +438,130 @@ const HackerView = ({ hackerData, presenceData: { id: _, hackerInfoId, ...presen
 					))}
 				</div>
 			</OnlyRole>
-			<h2 className="self-center font-[Coolvetica] text-2xl font-normal text-dark">General Information</h2>
-			<p className={paragraphClass}>
-				<b className={boldClass}>Contact Information</b> {hackerData.email} &mdash; {hackerData.phoneNumber}
-			</p>
-			<p className={paragraphClass}>
-				<b className={boldClass}>Emergency Contact</b> {hackerData.emergencyContactName} (
-				{hackerData.emergencyContactRelationship}) {hackerData.emergencyContactPhoneNumber}
-			</p>
-			<p className={paragraphClass}>
-				<b className={boldClass}>Requirements</b>
-				{hackerData.dietaryRestrictions
-					? hackerData.dietaryRestrictions
-					: "No Dietary Restrictions"} &mdash;{" "}
-				{hackerData.accessibilityRequirements
-					? hackerData.accessibilityRequirements
-					: "No Accessibility Requirements"}{" "}
-				&mdash; {hackerData.preferredLanguage} &mdash; {hackerData.shirtSize}
-			</p>
-			<p className={paragraphClass}>
-				<b className={boldClass}>Attendance</b>
-				{hackerData.confirmed ? "Confirmed attendance" : "Unconfirmed attendance"}
-				{hackerData.walkIn ? " (walk-in)" : ""} &mdash;{" "}
-				{hackerData.unsubscribed ? "Unsubscribed from emails" : "Subscribed to emails"} &mdash; Attends{" "}
-				{hackerData.attendanceType} at {hackerData.location} with{" "}
-				{hackerData.transportationRequired ? "" : "no"} transportation required
-			</p>
-			<p className="flex flex-row flex-wrap gap-4">
-				{Object.entries({
-					Resume: hackerData.linkResume,
-					LinkedIn: hackerData.linkLinkedin,
-					GitHub: hackerData.linkGithub,
-					"Personal Website": hackerData.linkPersonalSite,
-				}).map(
-					([key, value]) =>
-						value && (
-							<a
-								key={key}
-								href={value}
-								target="_blank"
-								rel="noreferrer"
-								className="flex items-center justify-center gap-2 rounded-md bg-gray-800 px-4 py-2 text-white hover:bg-gray-700"
-							>
-								{key}
-							</a>
-						),
+
+			<form onSubmit={handleSubmit}>
+				{Object.keys(groupedData).map((category, index) => (
+					<div key={index}>
+						<div className="flex justify-center">
+							<h2 className="self-center px-3 py-2 font-[Coolvetica] text-2xl font-normal text-dark">
+								{category}
+							</h2>
+						</div>
+						{groupedData[category]?.map((item, itemIndex) => (
+							<div key={itemIndex} className={paragraphClass}>
+								<b className={boldClass}>{item.label}</b>
+								{item.type === "select" ? (
+									<select
+										id={item.name}
+										name={item.name}
+										className="col-md-6 w-[50%] rounded-[100px] border-none bg-background1 px-5 py-2 font-rubik text-dark shadow-md transition-all duration-500 hover:bg-background1/50"
+										value={inputValues[item.name] || ""}
+										onChange={e => {
+											handleInputChange(item.name, e.target.value);
+										}}
+									>
+										{item.options?.map(option => (
+											<option key={option} value={option}>
+												{t(option)}
+											</option>
+										))}
+									</select>
+								) : (
+									<input
+										id={item.name}
+										name={item.name}
+										type={item.type}
+										className="/50 w-[50%] rounded-[100px] border-none bg-background1 px-4 py-2 font-rubik text-dark shadow-md outline-none	transition-all duration-500 hover:bg-background2"
+										defaultValue={
+											item.default_value !== null
+												? item.default_value
+												: item.type === "select"
+												? ""
+												: `No ${item.label}`
+										}
+										value={inputValues[item.name] || ""}
+										onChange={e => {
+											handleInputChange(item.name, e.target.value);
+										}}
+										pattern={patterns[item.type]}
+									/>
+								)}
+							</div>
+						))}
+					</div>
+				))}
+
+				<div className="flex justify-between gap-8 py-4 ">
+					<input className="rounded-md border border-gray-400 bg-background2 p-2" type="file" />
+					<button className="ml-2 rounded-md bg-dark px-4 py-2 text-white hover:bg-gray-700">
+						{t("uploadResume")}
+					</button>
+				</div>
+
+				<p className="flex flex-row flex-wrap justify-center gap-4 py-4">
+					{Object.entries({
+						Resume: hackerData.linkResume,
+						LinkedIn: hackerData.linkLinkedin,
+						GitHub: hackerData.linkGithub,
+						"Personal Website": hackerData.linkPersonalSite,
+					}).map(
+						([key, value]) =>
+							value && (
+								<a
+									key={key}
+									href={value}
+									target="_blank"
+									rel="noreferrer"
+									className="flex items-center justify-center gap-2 rounded-md bg-dark px-4 py-2 text-white hover:bg-gray-700"
+								>
+									{key}
+								</a>
+							),
+					)}
+				</p>
+
+				<OnlyRole filter={role => role === Role.ORGANIZER}>
+					<>
+						<div className="flex justify-center py-4">
+							<h2 className="self-center py-4 font-[Coolvetica] text-2xl font-normal text-dark ">
+								Debug Information
+							</h2>
+						</div>
+						<p className={paragraphClass}>
+							<b className={boldClass}>HackerInfo ID</b> {hackerData.id ?? "NULL"}
+						</p>
+						<p className={paragraphClass}>
+							<b className={boldClass}>User ID</b> {hackerData.userId ?? "NULL"}
+						</p>
+						<p className={paragraphClass}>
+							<b className={boldClass}>Unsubscribe Token</b> {hackerData.unsubscribeToken ?? "NULL"}
+						</p>
+						<p className={paragraphClass}>
+							<b className={boldClass}>Acceptance Expiry</b>{" "}
+							{(hackerData.acceptanceExpiry ?? "NULL").toString()}
+						</p>
+					</>
+				</OnlyRole>
+
+				{edit && (
+					<div className="sticky bottom-0 mx-2 flex justify-center">
+						<div className="flex max-w-md rounded-md bg-dark px-2 py-2 text-white transition delay-150 ease-in-out">
+							<div className="flex flex-col items-center gap-2">
+								<p className="px-5 py-2 text-center">{t("edit_description")}</p>
+							</div>
+							<div className="flex items-center justify-center">
+								<button className="px-4 py-2" onClick={resetInputFields}>
+									{t("edit_reset_button")}
+								</button>
+								<button className="h-max w-max rounded-md bg-green-700 px-4 py-2">
+									<i className="fas fa-user-edit"></i>
+									{t("edit_save_button")}
+								</button>
+							</div>
+						</div>
+					</div>
 				)}
-			</p>
-			<OnlyRole filter={role => role === Role.ORGANIZER}>
-				<>
-					<h2 className="self-center font-[Coolvetica] text-2xl font-normal text-dark">Debug Information</h2>
-					<p className={paragraphClass}>
-						<b className={boldClass}>HackerInfo ID</b> {hackerData.id ?? "NULL"}
-					</p>
-					<p className={paragraphClass}>
-						<b className={boldClass}>User ID</b> {hackerData.userId ?? "NULL"}
-					</p>
-					<p className={paragraphClass}>
-						<b className={boldClass}>Unsubscribe Token</b> {hackerData.unsubscribeToken ?? "NULL"}
-					</p>
-					<p className={paragraphClass}>
-						<b className={boldClass}>Acceptance Expiry</b>{" "}
-						{(hackerData.acceptanceExpiry ?? "NULL").toString()}
-					</p>
-				</>
-			</OnlyRole>
+			</form>
 		</>
 	);
 };
