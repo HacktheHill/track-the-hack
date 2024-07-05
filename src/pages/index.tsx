9751e15d43fd70fa72044b9a9e77e9554595c18c
Daniel Thorp <arcanistzed@gmail.com>
2024-07-05 08:18:47 -0400

    fix: icons and ui styling
    Signed-off-by: Daniel Thorp <arcanistzed@gmail.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,26 +19,26 @@ const Home: NextPage = () => {
 
 	return (
 		<NextUIProvider>
-			<App className="relative flex flex-col items-center justify-center gap-2 overflow-clip bg-default-gradient px-8 py-6 sm:gap-8 short:px-16 short:py-12">
-				<Image
-					priority
-					className="z-10"
-					src="/assets/mascot-waving.svg"
-					alt="Mascot"
-					width={225}
-					height={225}
-				/>
-				<p className="z-10 text-center text-lg font-bold short:text-2xl">{t("welcome")}</p>
-				<p className="z-10 max-w-xl text-center text-sm short:text-base">{t("explanation")}</p>
-				{!sessionData && (
-					<button
-						className="z-10 whitespace-nowrap rounded-lg border border-dark-primary-color bg-light-quaternary-color px-4 py-2 font-coolvetica text-sm text-dark-color transition-colors hover:bg-light-tertiary-color short:text-base"
-						onClick={() => void signIn()}
-					>
-						{t("get-started")}
-					</button>
-				)}
-			</App>
+		<App className="relative flex flex-col items-center justify-center gap-2 overflow-clip bg-default-gradient px-8 py-6 sm:gap-8 short:px-16 short:py-12">
+			<Image
+				priority
+				className="z-10"
+				src="/assets/mascot-waving.svg"
+				alt="Mascot"
+				width={225}
+				height={225}
+			/>
+			<p className="z-10 text-center text-lg font-bold short:text-2xl">{t("welcome")}</p>
+			<p className="z-10 max-w-xl text-center text-sm short:text-base">{t("explanation")}</p>
+			{!sessionData && (
+				<button
+					className="z-10 whitespace-nowrap rounded-lg border border-dark-primary-color bg-light-quaternary-color px-4 py-2 font-coolvetica text-sm text-dark-primary-color transition-colors hover:bg-light-tertiary-color short:text-base"
+					onClick={() => void signIn()}
+				>
+					{t("get-started")}
+				</button>
+			)}
+		</App>
 		</NextUIProvider>
 	);
 };
