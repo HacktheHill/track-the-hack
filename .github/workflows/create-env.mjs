74664adf09642b828064bf4d568c5a8ff040130d
Daniel Thorp <arcanistzed@gmail.com>
2024-07-09 17:56:27 -0400

    ci: remove _ secret prefixes so GITHUB_ works

--- .github/workflows/create-env.mjs
+++ .github/workflows/create-env.mjs
@@ -8,7 +8,7 @@ const env = JSON.parse(process.argv[2]);
 console.log(process.argv, env);
 
 const envFile = Object.keys(env)
-    .map((key) => `${key}=${env[key]}`)
+    .map((key) => `${key.replace(/^_+/, "")}=${env[key]}`)
     .join("\n");
 
 writeFileSync(".env", envFile);
