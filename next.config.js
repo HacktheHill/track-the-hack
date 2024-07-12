fef6685182e1ab31125deb766ab2859233289803
Daniel Thorp <arcanistzed@gmail.com>
2024-07-12 05:57:34 -0400

    ci: fix pwa warning message in dev
    Signed-off-by: Daniel Thorp <arcanistzed@gmail.com>

--- next.config.js
+++ next.config.js
@@ -12,7 +12,7 @@ const { i18n } = require("./next-i18next.config.js");
 const withPWA = require("next-pwa")({
 	dest: "public",
 	register: true,
-	disable: false,
+	disable: process.env.NODE_ENV === "development",
 });
 
 module.exports = withPWA({
