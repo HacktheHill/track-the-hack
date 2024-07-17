dd5160edfcaf99ed869946469a06927fb6b24c6d
Daniel Thorp <arcanistzed@gmail.com>
2024-07-17 13:46:38 -0400

    fix: try/catch to avoid clientside error messages
    Signed-off-by: Daniel Thorp <arcanistzed@gmail.com>

--- src/client/s3.ts
+++ src/client/s3.ts
@@ -2,13 +2,13 @@ const uploadResume = async (presignedUrl: string, file: File, name: string) => {
 	const body = new FormData();
 	body.append("file", file, name);
 
-	const response = await fetch(presignedUrl, {
-		method: "PUT",
-		body,
-	});
-
-	if (!response.ok) {
-		throw new Error("Failed to upload resume");
+	try {
+		await fetch(presignedUrl, {
+			method: "PUT",
+			body,
+		});
+	} catch (error) {
+		console.error("Failed to upload resume", error);
 	}
 
 	console.log("Uploaded resume to S3", name);
