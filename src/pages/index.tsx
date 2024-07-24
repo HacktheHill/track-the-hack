b8a48d99fc83a8e5997de48cbbfc399aa5d4a137
Daniel Thorp <arcanistzed@gmail.com>
2024-07-23 21:13:12 -0400

    feat: full sign up page
    Signed-off-by: Daniel Thorp <arcanistzed@gmail.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import type { GetStaticProps } from "next";
 import { type NextPage } from "next";
-import { signIn, useSession } from "next-auth/react";
+import { useSession } from "next-auth/react";
 import { useTranslation } from "next-i18next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import Image from "next/image";
@@ -20,34 +20,27 @@ const Home: NextPage = () => {
 	const router = useRouter();
 
 	return (
-			<App className="relative flex flex-col items-center justify-center gap-2 overflow-clip bg-default-gradient px-8 py-6 sm:gap-8 short:px-16 short:py-12">
-				<Image
-					priority
-					className="z-10"
-					src="/assets/mascot-waving.svg"
-					alt="Mascot"
-					width={225}
-					height={225}
-				/>
-				<p className="z-10 text-center text-lg font-bold short:text-2xl">{t("welcome")}</p>
-				{sessionData ? (
-					<Filter method="none" silent>
-						<button
-							className="z-10 whitespace-nowrap rounded-lg border border-dark-primary-color bg-light-quaternary-color px-4 py-2 font-coolvetica text-sm text-dark-primary-color transition-colors hover:bg-light-tertiary-color short:text-base"
-							onClick={() => void router.push("/apply")}
-						>
-							{t("apply")}
-						</button>
-					</Filter>
-				) : (
+		<App className="relative flex flex-col items-center justify-center gap-2 overflow-clip bg-default-gradient px-8 py-6 sm:gap-8 short:px-16 short:py-12">
+			<Image priority className="z-10" src="/assets/mascot-waving.svg" alt="Mascot" width={225} height={225} />
+			<p className="z-10 text-center text-lg font-bold short:text-2xl">{t("welcome")}</p>
+			{sessionData ? (
+				<Filter method="none" silent>
 					<button
 						className="z-10 whitespace-nowrap rounded-lg border border-dark-primary-color bg-light-quaternary-color px-4 py-2 font-coolvetica text-sm text-dark-primary-color transition-colors hover:bg-light-tertiary-color short:text-base"
-						onClick={() => void signIn()}
+						onClick={() => void router.push("/apply")}
 					>
-						{t("get-started")}
+						{t("apply")}
 					</button>
-				)}
-			</App>
+				</Filter>
+			) : (
+				<button
+					className="z-10 whitespace-nowrap rounded-lg border border-dark-primary-color bg-light-quaternary-color px-4 py-2 font-coolvetica text-sm text-dark-primary-color transition-colors hover:bg-light-tertiary-color short:text-base"
+					onClick={() => void router.push("/auth/sign-up")}
+				>
+					{t("get-started")}
+				</button>
+			)}
+		</App>
 	);
 };
 
