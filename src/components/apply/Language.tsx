d4816238d0882e4e2984daeeb463c02bb4275acf
Daniel Thorp <arcanistzed@gmail.com>
2024-07-26 14:24:56 -0400

    feat: session storage and refactor to reduce complexity
    Signed-off-by: Daniel Thorp <arcanistzed@gmail.com>

---
+++ src/components/apply/Language.tsx
@@ -0,0 +1,38 @@
+import { useRouter } from "next/router";
+import { useTranslation } from "next-i18next";
+import type { ProcessedFieldGeneric } from "../../server/lib/apply";
+
+const Language = ({ field }: { field: ProcessedFieldGeneric<"radio"> }) => {
+	const router = useRouter();
+	const { t } = useTranslation("apply");
+
+	return (
+		<div className="flex justify-evenly gap-4">
+			{Object.entries(field.options).map(([key, value]) => (
+				<div key={key} className="flex">
+					<input
+						type="radio"
+						id={`${field.name}-${key}`}
+						name={field.name}
+						value={key}
+						checked={router.locale === key.toLocaleLowerCase()}
+						className="peer hidden"
+						onChange={() => {
+							void router.push(router.pathname, router.pathname, {
+								locale: key.toLocaleLowerCase(),
+							});
+						}}
+					/>
+					<label
+						htmlFor={`${field.name}-${key}`}
+						className="cursor-pointer whitespace-nowrap rounded-lg border border-dark-primary-color bg-light-quaternary-color px-4 py-2 font-coolvetica text-4xl text-dark-primary-color transition-colors hover:bg-light-tertiary-color peer-checked:bg-light-primary-color/50"
+					>
+						{t(value)}
+					</label>
+				</div>
+			))}
+		</div>
+	);
+};
+
+export default Language;
---
+++ src/components/apply/Select.tsx
@@ -0,0 +1,62 @@
+import { useState, useEffect } from "react";
+import { useTranslation } from "next-i18next";
+import type { ProcessedFieldGeneric } from "../../server/lib/apply";
+
+const Select = ({
+	field,
+	className,
+	formData,
+}: {
+	field: ProcessedFieldGeneric<"select">;
+	className: string;
+	formData: FormData;
+}) => {
+	const { t } = useTranslation("apply");
+	const [showOther, setShowOther] = useState(false);
+	const [value, setValue] = useState<string | undefined>();
+	const [otherValue, setOtherValue] = useState<string | undefined>();
+
+	useEffect(() => {
+		const initialValue = formData.get(field.name)?.toString() ?? "";
+		setValue(initialValue);
+		const otherValue = formData.get(`${field.name}-other`)?.toString();
+		if (initialValue === "other" && otherValue) {
+			setShowOther(true);
+			setOtherValue(otherValue);
+		}
+	}, [field.name, formData]);
+
+	return (
+		<>
+			<select
+				id={field.name}
+				name={field.name}
+				className={className}
+				required={field.required}
+				value={value ?? ""}
+				onChange={e => {
+					setValue(e.target.value);
+					setShowOther(e.target.value === "other");
+				}}
+			>
+				<option value="">{t("select")}</option>
+				{Object.entries(field.options).map(([key, value]: [string, string]) => (
+					<option key={key} value={key}>
+						{t(value)}
+					</option>
+				))}
+			</select>
+			{field.options.other && showOther && (
+				<input
+					id={`${field.name}-other`}
+					name={`${field.name}-other`}
+					type="text"
+					className={className}
+					defaultValue={otherValue}
+				/>
+			)}
+		</>
+	);
+};
+
+export default Select;
