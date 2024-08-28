7012d7823d2260e7cfb494f909c1a90f68787ec8
Patrick <pigir101@uottawa.ca>
2024-08-28 16:15:32 -0400

    feat: handleMealevents

--- src/pages/qr/index.tsx
+++ src/pages/qr/index.tsx
@@ -15,9 +15,12 @@ import QRScanner from "../../components/QRScanner";
 import { trpc } from "../../server/api/api";
 import type { AppRouter } from "../../server/api/root";
 import type { inferRouterOutputs } from "@trpc/server";
+import { faker } from "@faker-js/faker";
+import { TRPCClientError } from "@trpc/client";
 
 type RouterOutput = inferRouterOutputs<AppRouter>;
 type Hacker = RouterOutput["hackers"]["get"];
+type Presence = RouterOutput["presence"]["getFromHackerId"];
 
 // TODO: sync it with db
 enum EVENTS {
@@ -41,37 +44,115 @@ const QR = () => {
 	const events = Object.keys(EVENTS) as EVENTS_T[];
 
 	const [hackerID, setHackerID] = useState<string>("");
-	const {data:hacker, refetch} = trpc.hackers.get.useQuery({ id: hackerID }, { enabled: false });
+	const {data:hacker, refetch:refetchHacker} = trpc.hackers.get.useQuery({ id: hackerID }, { enabled: false });
+	const {data:presences, refetch:refetchPresences} = trpc.presence.getFromHackerId.useQuery({ id: hackerID }, { enabled: false });
+
+	const presenceUpsertMutation = trpc.presence.upsert.useMutation();
+	const presenceIncrementMutation = trpc.presence.increment.useMutation();
 
 	const [display, setDisplay] = useState(<></>);
+
+	const handleMealEvent = async (event: EVENTS, hacker:Hacker, presences: Presence | undefined) => {
+		// Hacker already registered -> increment
+		if (presences && presences.length > 0) {
+			for (const presence of presences) {
+				if (presence.label === event) {
+					try {
+						await presenceIncrementMutation.mutateAsync({key: presence.key});
+						return <div>{hacker.firstName} has checked-in {presence.value + 1} times!</div>;
+					} catch (error) {
+						if (error instanceof TRPCClientError) {
+							return <div>Error: {error.message}</div>;
+						}
+						return <div>An unknown error occurred!</div>;
+					}
+				}
+			}
+		}
+	
+		// First check-in
+		try {
+			await presenceUpsertMutation.mutateAsync({key: faker.helpers.slugify(faker.lorem.words(2)), value: 1, label: event})
+			return <div>{hacker.firstName} successfully check-in for {event}</div>;
+		} catch (error) {
+			if (error instanceof TRPCClientError) {
+				return <div>Error: {error.message}</div>;
+			}
+			return <div>An unknown error occurred!</div>;
+		}
+	}
 
 	useEffect(() => {
 		console.info("Hacker ID: ", hackerID);
 		(() =>{
 			if (!hackerID || hackerID === "") return;
-			void refetch();
+			void refetchHacker();
+
+			if (selectedEvent != EVENTS.GET_HACKER) void refetchPresences();
 		})()
 	}, [hackerID]);
 
 	useEffect(() => {
-		if (!hacker) return;
+		async function handleEvenet () {
+			if (!hacker) return;
 
-		switch (selectedEvent) {
+			switch (selectedEvent) {
 			case EVENTS.NONE:
 				break;
 			case EVENTS.GET_HACKER:
-				setDisplay(<Card hacker={hacker} />);
+				setDisplay(
+				<div
+					className="hover:bg-medium block w-full rounded-lg bg-medium-primary-color p-6 text-light-color shadow"
+				>
+					<h3 className="text-2xl font-bold tracking-tight">{`${hacker.firstName} ${hacker.lastName}`}</h3>
+					<p>{hacker.major}</p>
+					<p>{hacker.currentSchoolOrganization}</p>
+					<p>{hacker.email}</p>
+					<p>{hacker.phoneNumber}</p>
+					<p>{hacker.gender}</p>
+					<p>{hacker.major}</p>
+				</div>);
 				break;
 			case EVENTS.OPENING_CEREMONY:
+				if (presences && presences.length > 0) {
+					for (const presence of presences) {
+						// Hacker already registered
+						if (presence.label === EVENTS.OPENING_CEREMONY) {
+							setDisplay(
+								<div>Error: hacker already checked in for opening ceremony!</div>
+							);
+							return
+						}
+					}
+				}
+				// Hacker not registered. TODO: is key randomly generated?
+				presenceUpsertMutation.mutate({key: faker.helpers.slugify(faker.lorem.words(2)), value: 1, label: EVENTS.OPENING_CEREMONY});
+				setDisplay(
+					<div>{hacker.firstName} successfully check-in for {EVENTS.OPENING_CEREMONY}</div>
+				);
+				break;
 			case EVENTS.FRIDAY_DINNER:
+				setDisplay(await handleMealEvent(EVENTS.FRIDAY_DINNER, hacker, presences));
+				break
 			case EVENTS.SATURDAY_BREAKFAST:
+				setDisplay(await handleMealEvent(EVENTS.SATURDAY_BREAKFAST, hacker, presences));
+				break;
 			case EVENTS.SATURDAY_LAUNCH:
+				setDisplay(await handleMealEvent(EVENTS.SATURDAY_LAUNCH, hacker, presences));
+				break;
 			case EVENTS.SATURDAY_DINNER:
+				setDisplay(await handleMealEvent(EVENTS.SATURDAY_DINNER, hacker, presences));
+				break;
 			case EVENTS.SUNDAY_BREAKFAST:
+				setDisplay(await handleMealEvent(EVENTS.SUNDAY_BREAKFAST, hacker, presences));
+				break;
 			default:
 				alert("Unhandled action!")
 				break;
+			}
 		}
+		void handleEvenet();
+		
 	}, [hacker]);
 
 	return (
@@ -119,23 +200,6 @@ const QR = () => {
 		</App>
 	);
 };
-
-
-function Card({hacker}:{hacker: Hacker}) {
-	if (!hacker) return <></>;
-
-	return <div
-		className="hover:bg-medium block w-full rounded-lg bg-medium-primary-color p-6 text-light-color shadow"
-	>
-		<h3 className="text-2xl font-bold tracking-tight">{`${hacker.firstName} ${hacker.lastName}`}</h3>
-		<p>{hacker.major}</p>
-		<p>{hacker.currentSchoolOrganization}</p>
-		<p>{hacker.email}</p>
-		<p>{hacker.phoneNumber}</p>
-		<p>{hacker.gender}</p>
-		<p>{hacker.major}</p>
-	</div>
-}
 
 export const getServerSideProps: GetServerSideProps = async ({ req, res, locale }) => {
 	const session = await getServerSession(req, res, getAuthOptions(req));
