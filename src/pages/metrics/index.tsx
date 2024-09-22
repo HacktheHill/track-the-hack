87042fdbd974f37816c2637aecd8855d76a1e311
Daniel Thorp <arcanistzed@gmail.com>
2024-09-22 18:01:57 -0400

    fix: metrics dashboard small horizontal screen size
    Signed-off-by: Daniel Thorp <arcanistzed@gmail.com>

--- src/pages/metrics/index.tsx
+++ src/pages/metrics/index.tsx
@@ -3,7 +3,8 @@ import type { GetServerSideProps } from "next";
 import { getServerSession } from "next-auth";
 import { useTranslation } from "next-i18next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
-import { Bar, BarChart, Cell, Pie, PieChart, Tooltip, XAxis, YAxis } from "recharts";
+import { useEffect, useState } from "react";
+import { Bar, BarChart, Cell, Pie, PieChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from "recharts";
 import { trpc } from "../../server/api/api";
 
 import App from "../../components/App";
@@ -12,6 +13,8 @@ import { getAuthOptions } from "../api/auth/[...nextauth]";
 
 const Metrics = () => {
 	const { t } = useTranslation("metrics");
+
+	const [pieRadius, setPieRadius] = useState(160);
 
 	const { data } = trpc.metrics.getMetrics.useQuery();
 
@@ -32,15 +35,21 @@ const Metrics = () => {
 		"#6a1b9a", // Deep Plum
 	];
 
+	useEffect(() => {
+		const isSmallScreen = window.innerWidth < 768;
+		const pieRadius = isSmallScreen ? 90 : 160;
+		setPieRadius(pieRadius);
+	}, []);
+
 	return (
 		<App className="overflow-y-auto bg-default-gradient" integrated={true} title={t("title")}>
-			<div className="flex flex-col items-center justify-center overflow-y-auto bg-default-gradient p-10">
+			<div className="flex flex-col items-center justify-center overflow-hidden overflow-y-auto bg-default-gradient p-10">
 				<h1 className="p-10 font-rubik text-4xl font-bold">{t("title")}</h1>
 
 				<div className="flex w-full max-w-6xl flex-col items-center gap-6 rounded-lg bg-light-tertiary-color p-6 shadow-lg">
 					<h3 className="font-coolvetica text-xl">{t("overview")}</h3>
 
-					<div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+					<div className="grid w-full grid-cols-1 gap-4 md:grid-cols-2">
 						{data && (
 							<>
 								<p className="text-lg">
@@ -59,171 +68,203 @@ const Metrics = () => {
 						)}
 
 						{data && (
-							<div>
+							<div className="w-full">
 								<h3 className="font-coolvetica text-xl">{t("attendance")}</h3>
-								<BarChart width={600} height={300} data={data.attendanceData}>
-									<XAxis dataKey="label" stroke="black" />
-									<YAxis stroke="black" />
-									<Tooltip />
-									<Bar dataKey="_sum.value" name={t("attendance")} stroke="black">
-										{data.genderData.map((_, index) => (
-											<Cell key={`cell-${index}`} fill={colors[index % colors.length]} />
-										))}
-									</Bar>
-								</BarChart>
+								<div className="p-4">
+									<ResponsiveContainer width="100%" height={300}>
+										<BarChart data={data.attendanceData}>
+											<XAxis dataKey="label" stroke="black" />
+											<YAxis stroke="black" />
+											<Tooltip />
+											<Bar dataKey="_sum.value" name={t("attendance")} stroke="black">
+												{data.genderData.map((_, index) => (
+													<Cell key={`cell-${index}`} fill={colors[index % colors.length]} />
+												))}
+											</Bar>
+										</BarChart>
+									</ResponsiveContainer>
+								</div>
 							</div>
 						)}
 					</div>
 
 					<div className="grid w-full grid-cols-1 gap-8 md:grid-cols-3">
 						{data && (
-							<div>
+							<div className="w-full max-w-[400px]">
 								<h3 className="font-coolvetica text-xl">{t("genderDistribution")}</h3>
-								<PieChart width={400} height={400}>
-									<Pie
-										data={data.genderData}
-										dataKey="_count.gender"
-										nameKey="gender"
-										cx="50%"
-										cy="50%"
-										outerRadius={150}
-										stroke="black"
-										fill="black"
-									>
-										{data.genderData.map((_, index) => (
-											<Cell key={`cell-${index}`} fill={colors[index % colors.length]} />
-										))}
-									</Pie>
-									<Tooltip />
-								</PieChart>
+								<ResponsiveContainer width="100%" height={400}>
+									<PieChart>
+										<Pie
+											data={data.genderData}
+											dataKey="_count.gender"
+											nameKey="gender"
+											cx="50%"
+											cy="50%"
+											outerRadius={pieRadius}
+											stroke="black"
+											fill="black"
+										>
+											{data.genderData.map((_, index) => (
+												<Cell key={`cell-${index}`} fill={colors[index % colors.length]} />
+											))}
+										</Pie>
+										<Tooltip />
+									</PieChart>
+								</ResponsiveContainer>
 							</div>
 						)}
 
 						{data && (
-							<div>
+							<div className="w-full max-w-[400px]">
 								<h3 className="font-coolvetica text-xl">{t("ethnicityDistribution")}</h3>
-								<PieChart width={400} height={400}>
-									<Pie
-										data={data.ethnicityData}
-										dataKey="_count.raceEthnicity"
-										nameKey="raceEthnicity"
-										cx="50%"
-										cy="50%"
-										outerRadius={150}
-										stroke="black"
-										fill="black"
-									>
-										{data.ethnicityData.map((_, index) => (
-											<Cell key={`cell-${index}`} fill={colors[index % colors.length]} />
-										))}
-									</Pie>
-									<Tooltip />
-								</PieChart>
+								<ResponsiveContainer width="100%" height={400}>
+									<PieChart>
+										<Pie
+											data={data.ethnicityData}
+											dataKey="_count.raceEthnicity"
+											nameKey="raceEthnicity"
+											cx="50%"
+											cy="50%"
+											outerRadius={pieRadius}
+											stroke="black"
+											fill="black"
+										>
+											{data.ethnicityData.map((_, index) => (
+												<Cell key={`cell-${index}`} fill={colors[index % colors.length]} />
+											))}
+										</Pie>
+										<Tooltip />
+									</PieChart>
+								</ResponsiveContainer>
 							</div>
 						)}
 
 						{data && (
-							<div>
+							<div className="w-full max-w-[400px]">
 								<h3 className="font-coolvetica text-xl">{t("ageDistribution")}</h3>
-								<PieChart width={400} height={400}>
-									<Pie
-										data={data.ageData}
-										dataKey="_count.age"
-										nameKey="age"
-										cx="50%"
-										cy="50%"
-										outerRadius={150}
-										stroke="black"
-										fill="black"
-									>
-										{data.ageData.map((_, index) => (
-											<Cell key={`cell-${index}`} fill={colors[index % colors.length]} />
-										))}
-									</Pie>
-									<Tooltip />
-								</PieChart>
+								<ResponsiveContainer width="100%" height={400}>
+									<PieChart>
+										<Pie
+											data={data.ageData}
+											dataKey="_count.age"
+											nameKey="age"
+											cx="50%"
+											cy="50%"
+											outerRadius={pieRadius}
+											stroke="black"
+											fill="black"
+										>
+											{data.ageData.map((_, index) => (
+												<Cell key={`cell-${index}`} fill={colors[index % colors.length]} />
+											))}
+										</Pie>
+										<Tooltip />
+									</PieChart>
+								</ResponsiveContainer>
 							</div>
 						)}
 					</div>
 
 					<div className="grid w-full grid-cols-1 gap-8 md:grid-cols-2">
 						{data && (
-							<div>
+							<div className="w-full">
 								<h3 className="font-coolvetica text-xl">{t("locationDistribution")}</h3>
-								<PieChart width={400} height={400}>
-									<Pie
-										data={data.locationData}
-										dataKey="_count.travelOrigin"
-										nameKey="travelOrigin"
-										cx="50%"
-										cy="50%"
-										outerRadius={150}
-										stroke="black"
-										fill="black"
-									>
-										{data.locationData.map((_, index) => (
-											<Cell key={`cell-${index}`} fill={colors[index % colors.length]} />
-										))}
-									</Pie>
-									<Tooltip />
-								</PieChart>
+								<ResponsiveContainer width="100%" height={400}>
+									<PieChart>
+										<Pie
+											data={data.locationData}
+											dataKey="_count.travelOrigin"
+											nameKey="travelOrigin"
+											cx="50%"
+											cy="50%"
+											outerRadius={pieRadius}
+											stroke="black"
+											fill="black"
+										>
+											{data.locationData.map((_, index) => (
+												<Cell key={`cell-${index}`} fill={colors[index % colors.length]} />
+											))}
+										</Pie>
+										<Tooltip />
+									</PieChart>
+								</ResponsiveContainer>
 							</div>
 						)}
 
 						{data && (
-							<div>
+							<div className="w-full">
 								<h3 className="font-coolvetica text-xl">{t("educationLevelDistribution")}</h3>
-								<PieChart width={400} height={400}>
-									<Pie
-										data={data.levelData}
-										dataKey="_count.educationLevel"
-										nameKey="educationLevel"
-										cx="50%"
-										cy="50%"
-										outerRadius={150}
-										stroke="black"
-										fill="black"
-									>
-										{data.levelData.map((_, index) => (
-											<Cell key={`cell-${index}`} fill={colors[index % colors.length]} />
-										))}
-									</Pie>
-									<Tooltip />
-								</PieChart>
+								<ResponsiveContainer width="100%" height={400}>
+									<PieChart>
+										<Pie
+											data={data.levelData}
+											dataKey="_count.educationLevel"
+											nameKey="educationLevel"
+											cx="50%"
+											cy="50%"
+											outerRadius={pieRadius}
+											stroke="black"
+											fill="black"
+										>
+											{data.levelData.map((_, index) => (
+												<Cell key={`cell-${index}`} fill={colors[index % colors.length]} />
+											))}
+										</Pie>
+										<Tooltip />
+									</PieChart>
+								</ResponsiveContainer>
 							</div>
 						)}
 					</div>
 
 					<div className="grid w-full grid-cols-1 gap-8 md:grid-cols-2">
 						{data && (
-							<div>
+							<div className="w-full">
 								<h3 className="font-coolvetica text-xl">{t("referralSource")}</h3>
-								<BarChart width={500} height={300} data={data.referralSourceData}>
-									<XAxis dataKey="referralSource" stroke="black" />
-									<YAxis stroke="black" />
-									<Tooltip />
-									<Bar dataKey="_count.referralSource" name={t("referralSource")} stroke="black">
-										{data.referralSourceData.map((_, index) => (
-											<Cell key={`cell-${index}`} fill={colors[index % colors.length]} />
-										))}
-									</Bar>
-								</BarChart>
+								<div className="p-4">
+									<ResponsiveContainer width="100%" height={300}>
+										<BarChart
+											data={data.referralSourceData}
+											margin={{ top: 20, right: 30, left: 0, bottom: 5 }}
+										>
+											<XAxis dataKey="referralSource" stroke="black" />
+											<YAxis stroke="black" />
+											<Tooltip />
+											<Bar
+												dataKey="_count.referralSource"
+												name={t("referralSource")}
+												stroke="black"
+											>
+												{data.referralSourceData.map((_, index) => (
+													<Cell key={`cell-${index}`} fill={colors[index % colors.length]} />
+												))}
+											</Bar>
+										</BarChart>
+									</ResponsiveContainer>
+								</div>
 							</div>
 						)}
 
 						{data && (
-							<div>
+							<div className="w-full">
 								<h3 className="font-coolvetica text-xl">{t("majorsDistribution")}</h3>
-								<BarChart width={500} height={300} data={data.majorData}>
-									<XAxis dataKey="major" stroke="black" />
-									<YAxis stroke="black" />
-									<Tooltip />
-									<Bar dataKey="_count.major" name={t("major")} stroke="black">
-										{data.majorData.map((_, index) => (
-											<Cell key={`cell-${index}`} fill={colors[index % colors.length]} />
-										))}
-									</Bar>
-								</BarChart>
+								<div className="p-4">
+									<ResponsiveContainer width="100%" height={300}>
+										<BarChart
+											data={data.majorData}
+											margin={{ top: 20, right: 30, left: 0, bottom: 5 }}
+										>
+											<XAxis dataKey="major" stroke="black" />
+											<YAxis stroke="black" />
+											<Tooltip />
+											<Bar dataKey="_count.major" name={t("major")} stroke="black">
+												{data.majorData.map((_, index) => (
+													<Cell key={`cell-${index}`} fill={colors[index % colors.length]} />
+												))}
+											</Bar>
+										</BarChart>
+									</ResponsiveContainer>
+								</div>
 							</div>
 						)}
 					</div>
