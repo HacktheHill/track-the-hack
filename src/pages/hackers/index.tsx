19eedeb316d8f12fcb247e416a97b29eb16302f9
Daniel Thorp <arcanistzed@gmail.com>
2024-09-22 06:17:12 -0400

    fix: no hackers found svg
    Signed-off-by: Daniel Thorp <arcanistzed@gmail.com>

--- src/pages/hackers/index.tsx
+++ src/pages/hackers/index.tsx
@@ -140,16 +140,16 @@ const Hackers: NextPage = () => {
 							major={hacker.major}
 						/>
 					))}
-					{hackers?.length === 0 && (
-						<div className="flex h-full w-full flex-col items-center justify-center gap-4 text-2xl text-dark-color">
-							<svg className="h-20 w-20" fill="currentColor" viewBox="0 0 24 24">
-								<path d="M10 0h24v24H0z" fill="none" />
-								<path d="M14 2C14 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-1 15h-2v-6h2v6zm0-8h-2V7h2v2z" />
-							</svg>
-							<p>{t("filterSection.noHackersFound")}</p>
-						</div>
-					)}
 				</div>
+				{hackers?.length === 0 && (
+					<div className="flex h-full w-full flex-col items-center justify-center gap-4 text-2xl text-dark-color m-4">
+						<svg className="h-20 w-20" fill="currentColor" viewBox="0 0 24 24">
+							<path d="M10 0h24v24H0z" fill="none" />
+							<path d="M14 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-1 15h-2v-6h2v6zm0-8h-2V7h2v2z" />
+						</svg>
+						<p>{t("filterSection.noHackersFound")}</p>
+					</div>
+				)}
 			</div>
 		</App>
 	);
