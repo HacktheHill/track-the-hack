e517f82c2de20b80bcf32fe76388f2112dd6572b
Daniel Thorp <arcanistzed@gmail.com>
2024-09-25 07:11:34 -0400

    feat: rotating and encrypted QR codes
    Signed-off-by: Daniel Thorp <arcanistzed@gmail.com>

--- src/components/QRScanner.tsx
+++ src/components/QRScanner.tsx
@@ -1,31 +1,42 @@
 import QrScanner from "qr-scanner";
-import { useEffect, useRef, memo } from "react";
+import { memo, useCallback, useEffect, useRef } from "react";
+import { trpc } from "../server/api/api";
 
 type QRScannerProps = {
 	onScan: (data: string) => void;
+	setError: (message: string) => void;
 };
 
 const QRScanner = memo(function QRScanner(props: QRScannerProps) {
 	const video = useRef<HTMLVideoElement>(null);
+	const { mutateAsync: decryptHackerId } = trpc.qr.decryptHackerId.useMutation();
+
+	const handleScan = useCallback(
+		async (data: string) => {
+			try {
+				const decryptedHackerId = await decryptHackerId(data);
+				if (!decryptedHackerId) return;
+				props.onScan(decryptedHackerId);
+				props.setError("");
+			} catch (error) {
+				console.error(error);
+				props.setError((error as Error).message);
+			}
+		},
+		[decryptHackerId, props],
+	);
 
 	useEffect(() => {
 		if (!video.current) return;
-		const qrScanner = new QrScanner(
-			video.current,
-			(result: QrScanner.ScanResult) => {
-				props.onScan(result.data);
-			},
-			{
-				returnDetailedScanResult: true,
-				highlightCodeOutline: true,
-			},
-		);
+
+		const qrScanner = new QrScanner(video.current, result => void handleScan(result.data), {});
 		void qrScanner.start();
 
 		return () => {
 			qrScanner.stop();
 		};
-	});
+	}, [decryptHackerId, handleScan]);
+
 	return <video ref={video} className="aspect-square rounded-3xl object-cover" width="300" height="300" />;
 });
 
