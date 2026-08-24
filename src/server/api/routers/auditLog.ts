ac5777d5c22cfc0e16b73968a1377f77d9b28e17
Daniel Thorp <arcanistzed@gmail.com>
2026-08-23 22:49:54 -0400

    fix(authz): protect audit log procedures

--- src/server/api/routers/auditLog.ts
+++ src/server/api/routers/auditLog.ts
@@ -1,8 +1,8 @@
-import { PrismaClient } from "@prisma/client";
+import { RoleName } from "@prisma/client";
+import { TRPCError } from "@trpc/server";
+import { hasRoles } from "../../../utils/helpers";
 import { z } from "zod";
 import { createTRPCRouter, protectedProcedure } from "../trpc";
-
-const prisma = new PrismaClient();
 
 export const logRouter = createTRPCRouter({
 	new: protectedProcedure
@@ -18,20 +18,64 @@ export const logRouter = createTRPCRouter({
 				sourceType: z.string(),
 			}),
 		)
-		.mutation(async ({ input }) => {
-			const log = await prisma.log.create({
+		.mutation(async ({ ctx, input }) => {
+			const userId = ctx.session.user.id;
+			const user = await ctx.prisma.user.findUnique({
+				where: {
+					id: userId,
+				},
+				select: {
+					roles: {
+						select: {
+							name: true,
+						},
+					},
+				},
+			});
+
+			if (!user) {
+				throw new TRPCError({ code: "NOT_FOUND", message: "User not found" });
+			}
+
+			if (!hasRoles(user, [RoleName.ADMIN, RoleName.ORGANIZER])) {
+				throw new TRPCError({ code: "FORBIDDEN", message: "You do not have permission to do this" });
+			}
+
+			const log = await ctx.prisma.log.create({
 				data: input,
 			});
 
 			if (!log) {
-				throw new Error("Audit Log unsuccessful");
+				throw new TRPCError({ code: "INTERNAL_SERVER_ERROR", message: "Audit Log unsuccessful" });
 			}
 
 			return log;
 		}),
 
-	all: protectedProcedure.query(async () => {
-		const logs = await prisma.log.findMany({
+	all: protectedProcedure.query(async ({ ctx }) => {
+		const userId = ctx.session.user.id;
+		const user = await ctx.prisma.user.findUnique({
+			where: {
+				id: userId,
+			},
+			select: {
+				roles: {
+					select: {
+						name: true,
+					},
+				},
+			},
+		});
+
+		if (!user) {
+			throw new TRPCError({ code: "NOT_FOUND", message: "User not found" });
+		}
+
+		if (!hasRoles(user, [RoleName.ADMIN, RoleName.ORGANIZER])) {
+			throw new TRPCError({ code: "FORBIDDEN", message: "You do not have permission to do this" });
+		}
+
+		const logs = await ctx.prisma.log.findMany({
 			orderBy: [
 				{
 					timestamp: "desc",
@@ -40,7 +84,7 @@ export const logRouter = createTRPCRouter({
 		});
 
 		if (!logs) {
-			throw new Error("No audit logs found");
+			throw new TRPCError({ code: "NOT_FOUND", message: "No audit logs found" });
 		}
 
 		return logs
